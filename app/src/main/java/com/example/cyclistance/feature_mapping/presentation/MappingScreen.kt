471eb86f3e15d59ef32ec62ee47f8cd0ff238b8b
John_Dominic <johndominicjasmin@gmail.com>
2022-03-02 22:24:38 +0800

    Display map

--- app/src/main/java/com/example/cyclistance/feature_mapping/presentation/MappingScreen.kt
+++ app/src/main/java/com/example/cyclistance/feature_mapping/presentation/MappingScreen.kt
@@ -1,5 +1,9 @@
 package com.example.cyclistance.feature_mapping.presentation
 
+import android.content.Context
+import android.view.LayoutInflater
+import android.view.View
+import android.widget.TextView
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.border
@@ -34,8 +38,8 @@ import com.example.cyclistance.feature_authentication.presentation.theme.ThemeCo
 import com.mapbox.mapboxsdk.Mapbox
 import com.mapbox.mapboxsdk.camera.CameraPosition
 import com.mapbox.mapboxsdk.camera.CameraUpdateFactory
-import com.mapbox.mapboxsdk.maps.MapView
 import com.mapbox.mapboxsdk.maps.Style
+import com.mapbox.maps.MapView
 
 
 @Preview
@@ -47,217 +51,220 @@ fun MappingScreen() {
 
 
     Scaffold(
+        drawerGesturesEnabled = false,
         scaffoldState = scaffoldState,
         topBar = {
-
             SetupTopAppBar(scaffoldState)
 
 
         },
         drawerContent = { SetupDrawerContent() },
     ) {
-        Text("Test")
+
+
+        AndroidView(
+            factory = { context: Context ->
+                val view =
+                    LayoutInflater.from(context).inflate(R.layout.activity_mapping, null, false)
+                val mapView: MapView = view.findViewById(R.id.mapView)
+                mapView.getMapboxMap().loadStyleUri(com.mapbox.maps.Style.DARK)
+                view
+            },
+            update = { view ->
+                view.visibility = View.VISIBLE
+
+            }
+        )
+
     }
 }
-
 
 @Preview
 @Composable
 fun SetupDrawerContent() {
 
 
-   ConstraintLayout(modifier = Modifier
-       .fillMaxSize()
-       .background(BackgroundColor), constraintSet = drawerMappingConstraintSet) {
+    ConstraintLayout(
+        modifier = Modifier
+            .fillMaxSize()
+            .background(BackgroundColor), constraintSet = drawerMappingConstraintSet) {
 
-       Box(
-           modifier = Modifier
-               .layoutId(MappingDrawerConstraintItem.UpperSection.layoutId)
-               .background(Color(0xFF3F3F3F))) {
-
-
-           Column(
-               modifier = Modifier.fillMaxSize(),
-               verticalArrangement = Arrangement.Center,
-               horizontalAlignment = Alignment.CenterHorizontally) {
+        Box(
+            modifier = Modifier
+                .layoutId(MappingDrawerConstraintItem.UpperSection.layoutId)
+                .background(Color(0xFF3F3F3F))) {
 
 
-               Image(
-                   painter = painterResource(R.drawable.mike_tyson),
-                   contentDescription = "User Picture",
-                   contentScale = Crop,
-                   modifier = Modifier
-                       .size(68.dp)
-                       .clip(CircleShape)
-                       .border(2.dp, ThemeColor, CircleShape)
-               )
-
-               Text(
-                   text = "John Doe",
-                   color = Color.White,
-                   style = MaterialTheme.typography.h6,
-                   modifier = Modifier.padding(top = 7.dp, bottom = 0.5.dp))
+            Column(
+                modifier = Modifier.fillMaxSize(),
+                verticalArrangement = Arrangement.Center,
+                horizontalAlignment = Alignment.CenterHorizontally) {
 
 
-               Text(
-                   text = "johndoe@gmail.com",
-                   color = Color(0xFFA5A5A5),
-                   style = MaterialTheme.typography.subtitle2)
+                Image(
+                    painter = painterResource(R.drawable.mike_tyson),
+                    contentDescription = "User Picture",
+                    contentScale = Crop,
+                    modifier = Modifier
+                        .size(68.dp)
+                        .clip(CircleShape)
+                        .border(2.dp, ThemeColor, CircleShape)
+                )
 
-           }
-       }
+                Text(
+                    text = "John Doe",
+                    color = Color.White,
+                    style = MaterialTheme.typography.h6,
+                    modifier = Modifier.padding(top = 7.dp, bottom = 0.5.dp))
 
 
+                Text(
+                    text = "johndoe@gmail.com",
+                    color = Color(0xFFA5A5A5),
+                    style = MaterialTheme.typography.subtitle2)
 
+            }
+        }
 
 
 
+        Column(
+            modifier = Modifier
+                .layoutId(MappingDrawerConstraintItem.BottomSection.layoutId),
+            horizontalAlignment = Alignment.CenterHorizontally,
+            verticalArrangement = Arrangement.spacedBy(space = 1.5.dp)
+        ) {
 
+            DrawerContentButton(
+                modifier = Modifier
+                    .background(Transparent)
+                    .fillMaxWidth()
+                    .wrapContentHeight(),
+                iconId = R.drawable.ic_baseline_person_outline_24,
+                buttonText = "Profile") { /* todo onClick here */ }
 
+            DrawerContentButton(
+                modifier = Modifier
+                    .background(Transparent)
+                    .fillMaxWidth()
+                    .wrapContentHeight(),
+                iconId = R.drawable.ic_baseline_settings_24,
+                buttonText = "Settings") { /* todo onClick here */ }
 
+            DrawerContentButton(
+                modifier = Modifier
+                    .background(Transparent)
+                    .fillMaxWidth()
+                    .wrapContentHeight(),
+                iconId = R.drawable.ic_baseline_chat_bubble_outline_24,
+                buttonText = "Chat") { /* todo onClick here */ }
 
+            DrawerContentButton(
+                modifier = Modifier
+                    .background(Transparent)
+                    .fillMaxWidth()
+                    .wrapContentHeight(),
+                iconId = R.drawable.ic_baseline_notifications_none_24,
+                buttonText = "Notifications") { /* todo onClick here */ }
 
-       Column(
-           modifier = Modifier
-               .layoutId(MappingDrawerConstraintItem.BottomSection.layoutId),
-           horizontalAlignment = Alignment.CenterHorizontally,
-           verticalArrangement = Arrangement.spacedBy(space = 1.5.dp)
-       ) {
-
-           DrawerContentButton(
-               modifier = Modifier
-                   .background(Transparent)
-                   .fillMaxWidth()
-                   .wrapContentHeight(),
-               iconId = R.drawable.ic_baseline_person_outline_24,
-               buttonText = "Profile") { /* todo onClick here */ }
-
-           DrawerContentButton(
-               modifier = Modifier
-                   .background(Transparent)
-                   .fillMaxWidth()
-                   .wrapContentHeight(),
-               iconId = R.drawable.ic_baseline_settings_24,
-               buttonText = "Settings") { /* todo onClick here */ }
-
-           DrawerContentButton(
-               modifier = Modifier
-                   .background(Transparent)
-                   .fillMaxWidth()
-                   .wrapContentHeight(),
-               iconId = R.drawable.ic_baseline_chat_bubble_outline_24,
-               buttonText = "Chat") { /* todo onClick here */ }
-
-           DrawerContentButton(
-               modifier = Modifier
-                   .background(Transparent)
-                   .fillMaxWidth()
-                   .wrapContentHeight(),
-               iconId = R.drawable.ic_baseline_notifications_none_24,
-               buttonText = "Notifications") { /* todo onClick here */ }
-
-           DrawerContentButton(
-               modifier = Modifier
-                   .background(Transparent)
-                   .fillMaxWidth()
-                   .wrapContentHeight(),
-               iconId = R.drawable.ic_about,
-               buttonText = "About") { /* todo onClick here */ }
+            DrawerContentButton(
+                modifier = Modifier
+                    .background(Transparent)
+                    .fillMaxWidth()
+                    .wrapContentHeight(),
+                iconId = R.drawable.ic_about,
+                buttonText = "About") { /* todo onClick here */ }
 
 
 
 
-           DrawerContentButton(
-               modifier = Modifier
-                   .background(Transparent)
-                   .fillMaxWidth()
-                   .wrapContentHeight(),
-               iconId = R.drawable.ic_group_35,
-               buttonText = "Sign out") { /* todo onClick here */ }
-       }
+            DrawerContentButton(
+                modifier = Modifier
+                    .background(Transparent)
+                    .fillMaxWidth()
+                    .wrapContentHeight(),
+                iconId = R.drawable.ic_group_35,
+                buttonText = "Sign out") { /* todo onClick here */ }
+        }
 
 
-   }
-
+    }
 
 
 }
 
 
-
-
 @Composable
 fun DrawerContentButton(
-   modifier: Modifier,
-   iconId: Int,
-   buttonText: String,
-   onClick: () -> Unit) {
+    modifier: Modifier,
+    iconId: Int,
+    buttonText: String,
+    onClick: () -> Unit) {
 
 
-   TextButton(
-       onClick = onClick,
-       modifier = modifier) {
+    TextButton(
+        onClick = onClick,
+        modifier = modifier) {
 
-       Icon(
-           painter = painterResource(id = iconId),
-           contentDescription = null,
-           modifier = Modifier
-               .align(Alignment.CenterVertically)
-               .weight(1f)
-               .size(26.dp), tint = Color(0xFFA6A6A6))
-
-
-       Text(
-           text = buttonText,
-           modifier = Modifier
-               .weight(10f)
-               .padding(start = 20.dp),
-           style = MaterialTheme.typography.subtitle1,
-           color = Color.White
-
-       )
+        Icon(
+            painter = painterResource(id = iconId),
+            contentDescription = null,
+            modifier = Modifier
+                .align(Alignment.CenterVertically)
+                .weight(1f)
+                .size(26.dp), tint = Color(0xFFA6A6A6))
 
 
-   }
+        Text(
+            text = buttonText,
+            modifier = Modifier
+                .weight(10f)
+                .padding(start = 20.dp),
+            style = MaterialTheme.typography.subtitle1,
+            color = Color.White
 
+        )
+
+
+    }
 
 
 }
 
 @Composable
 fun SetupTopAppBar(scaffoldState: ScaffoldState) {
-   val coroutineScope = rememberCoroutineScope()
-   TopAppBar(
-       elevation = 10.dp,
-       title = {
-           Row(
-               modifier = Modifier.wrapContentSize(),
-               verticalAlignment = Alignment.CenterVertically,
-               horizontalArrangement = Arrangement.spacedBy(5.dp)) {
+    val coroutineScope = rememberCoroutineScope()
+    TopAppBar(
+        elevation = 10.dp,
+        title = {
+            Row(
+                modifier = Modifier.wrapContentSize(),
+                verticalAlignment = Alignment.CenterVertically,
+                horizontalArrangement = Arrangement.spacedBy(5.dp)) {
 
-               Image(
-                   painter = painterResource(id = R.drawable.ic_cyclistance_app_icon),
-                   contentDescription = "Image Icon",
-                   alignment = Alignment.Center,
-                   modifier = Modifier
-                       .size(30.dp)
-                       .align(alignment = Alignment.CenterVertically)
-                       .padding(top = 5.dp)
-               )
-               Text(
-                   "Cyclistance",
-                   color = Color.White,
-                   fontWeight = FontWeight.Medium,
-                   fontSize = 20.sp)
-           }
-       },
-       backgroundColor = TopAppBarBackgroundColor,
-       navigationIcon = {
-           IconButton(onClick = { coroutineScope.launch { scaffoldState.drawerState.open() } }
-           ) {
-               Icon(Icons.Filled.Menu, contentDescription = "Menu Icon", tint = Color.White)
-           }
-       })
+                Image(
+                    painter = painterResource(id = R.drawable.ic_cyclistance_app_icon),
+                    contentDescription = "Image Icon",
+                    alignment = Alignment.Center,
+                    modifier = Modifier
+                        .size(30.dp)
+                        .align(alignment = Alignment.CenterVertically)
+                        .padding(top = 5.dp)
+                )
+                Text(
+                    "Cyclistance",
+                    color = Color.White,
+                    fontWeight = FontWeight.Medium,
+                    fontSize = 20.sp)
+            }
+        },
+        backgroundColor = TopAppBarBackgroundColor,
+        navigationIcon = {
+            IconButton(onClick = { coroutineScope.launch { scaffoldState.drawerState.open() } }
+            ) {
+                Icon(Icons.Filled.Menu, contentDescription = "Menu Icon", tint = Color.White)
+            }
+        })
 
 }
 
