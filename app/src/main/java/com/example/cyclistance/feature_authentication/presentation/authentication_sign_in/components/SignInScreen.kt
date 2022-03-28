d7e0d0575b71eee894710e600dbb5e281ca00481
John_Dominic <johndominicjasmin@gmail.com>
2022-03-28 23:45:17 +0800

    Setup Sign in Screen.

--- app/src/main/java/com/example/cyclistance/feature_authentication/presentation/authentication_sign_in/components/SignInScreen.kt
+++ app/src/main/java/com/example/cyclistance/feature_authentication/presentation/authentication_sign_in/components/SignInScreen.kt
@@ -1,25 +1,20 @@
 package com.example.cyclistance.feature_authentication.presentation.authentication_sign_in.components
 
-import android.widget.Toast
-import androidx.annotation.RawRes
-import androidx.compose.foundation.BorderStroke
 import androidx.compose.foundation.background
-import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.*
-import androidx.compose.foundation.shape.CircleShape
-import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.*
 
 import androidx.compose.runtime.*
+import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-
-import androidx.compose.ui.platform.LocalContext
 
 import androidx.compose.ui.text.input.TextFieldValue
 import androidx.constraintlayout.compose.ConstraintLayout
 import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.navigation.NavController
+import com.example.cyclistance.common.AlertDialogData
+import com.example.cyclistance.common.SetupAlertDialog
 
 import com.example.cyclistance.feature_authentication.domain.model.AuthModel
 import com.example.cyclistance.feature_authentication.presentation.authentication_email.EmailAuthViewModel
@@ -33,60 +28,23 @@ import com.example.cyclistance.feature_authentication.presentation.theme.*
 
 @Composable
 fun SignInScreen(
-    navController: NavController?) {
+    navController: NavController?,
+    signInViewModel: SignInViewModel = hiltViewModel(),
+    emailAuthViewModel: EmailAuthViewModel = hiltViewModel()
+) {
 
-    val signInViewModel: SignInViewModel = hiltViewModel()
-    val signUpViewModel: SignUpViewModel = hiltViewModel()
-    val emailAuthViewModel: EmailAuthViewModel = hiltViewModel()
 
-    val email = remember { mutableStateOf(TextFieldValue("")) }
-    val password = remember { mutableStateOf(TextFieldValue("")) }
 
+    val email = rememberSaveable(stateSaver = TextFieldValue.Saver) { mutableStateOf(TextFieldValue("")) }
+    val password = rememberSaveable(stateSaver = TextFieldValue.Saver) { mutableStateOf(TextFieldValue("")) }
     val signInState by remember { signInViewModel.signInWithEmailAndPasswordState }
-    val emailAuthState by remember { emailAuthViewModel.reloadState }
-    val isEmailVerified by remember { emailAuthViewModel.isEmailVerified }
+    val emailReloadState by remember { emailAuthViewModel.reloadEmailState }
+    val emailVerifyState by remember { emailAuthViewModel.verifyEmailState }
 
 
-    signInState.authState.result?.let { signInIsSuccessful ->
-        LaunchedEffect(key1 = signInIsSuccessful) {
-            if (signInIsSuccessful) {
-                signUpViewModel.saveAccount()
-                emailAuthViewModel.reloadEmail()
-
-            }
-
-        }
-    }
-
-    emailAuthState.result?.let { reloadEmailIsSuccessful ->
-        isEmailVerified?.let { emailVerificationIsSuccessful ->
-            LaunchedEffect(key1 = reloadEmailIsSuccessful, key2 = emailVerificationIsSuccessful) {
-                if (reloadEmailIsSuccessful && emailVerificationIsSuccessful) {
-                    navController?.navigate(Screens.MappingScreen.route) {
-                        popUpTo(Screens.SignInScreen.route) { inclusive = true }
-                        launchSingleTop = true
-                    }
-
-                } else {
-                    navController?.navigate(Screens.EmailAuthScreen.route) {
-                        popUpTo(Screens.SignInScreen.route) { inclusive = true }
-                        launchSingleTop = true
-                    }
-                }
-            }
-        }
-    }
 
 
-    if (signInState.internetExceptionMessage.isNotEmpty()) {
-        Toast.makeText(LocalContext.current, "No internet", Toast.LENGTH_SHORT).show()
-    }
-    if (signInState.invalidUserExceptionMessage.isNotEmpty()) {
-        Toast.makeText(LocalContext.current, "Invalid User.", Toast.LENGTH_SHORT).show()
-    }
-    if (signInState.userCollisionExceptionMessage.isNotEmpty()) {
-        Toast.makeText(LocalContext.current, "User Collide", Toast.LENGTH_SHORT).show()
-    }
+
 
     Column(
 
@@ -109,6 +67,71 @@ fun SignInScreen(
                 topWaveLayoutId = AuthenticationConstraintsItem.TopWave.layoutId,
                 bottomWaveLayoutId = AuthenticationConstraintsItem.BottomWave.layoutId
             )
+
+
+
+
+            emailReloadState.result?.let { reloadEmailIsSuccessful ->
+                if (reloadEmailIsSuccessful) {
+                    emailAuthViewModel.verifyEmail()
+                }
+            }
+
+
+            signInState.internetExceptionMessage.let { message ->
+                if (message.isNotEmpty()) {
+                    navController?.navigate(Screens.NoInternetScreen.route) {
+                        launchSingleTop = true
+                    }
+                }
+            }
+
+
+
+            emailVerifyState.result?.let { isSuccessful ->
+                if (isSuccessful) {
+                    navController?.navigate(Screens.MappingScreen.route) {
+                        popUpTo(Screens.SignInScreen.route) { inclusive = true }
+                        launchSingleTop = true
+                    }
+                } else {
+                    navController?.navigate(Screens.EmailAuthScreen.route) {
+                        popUpTo(Screens.SignInScreen.route) { inclusive = true }
+                        launchSingleTop = true
+                    }
+                }
+            }
+
+            signInState.result?.let { signInIsSuccessful ->
+                if (signInIsSuccessful) {
+                    emailAuthViewModel.reloadEmail()
+                }
+            }
+
+
+            signInState.invalidUserExceptionMessage.let{ message ->
+                if(message.isNotEmpty()){
+                    SetupAlertDialog(
+                        alertDialog = AlertDialogData(
+                            title = "Error",
+                            description = message,
+                            resId = io.github.farhanroy.composeawesomedialog.R.raw.error))
+                }
+            }
+
+            signInState.userCollisionExceptionMessage.let { message ->
+                if(message.isNotEmpty()){
+                    SetupAlertDialog(
+                        alertDialog = AlertDialogData(
+                            title = "Error",
+                            description = message,
+                            resId = io.github.farhanroy.composeawesomedialog.R.raw.error))
+                }
+            }
+
+
+
+
 
             SignInTextFieldsSection(
                 email = email,
@@ -151,7 +174,7 @@ fun SignInScreen(
 
         }
 
-        if (signInState.authState.isLoading || emailAuthState.isLoading) {
+        if (signInState.isLoading || emailReloadState.isLoading) {
             CircularProgressIndicator(
                 modifier = Modifier.align(alignment = Alignment.CenterHorizontally)
             )
