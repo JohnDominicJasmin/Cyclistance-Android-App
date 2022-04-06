fb6204fd5f268a736b33de66238dc93388ce3bbf
John_Dominic <johndominicjasmin@gmail.com>
2022-04-06 15:20:29 +0800

    Add swipe to refresh.

---
+++ app/src/main/java/com/example/cyclistance/common/SwipeToRefresh.kt
@@ -0,0 +1,80 @@
+package com.example.cyclistance.common
+
+import androidx.compose.animation.core.FastOutSlowInEasing
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.material.LinearProgressIndicator
+import androidx.compose.material.MaterialTheme
+import androidx.compose.runtime.*
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.drawWithCache
+import androidx.compose.ui.graphics.Brush
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.LocalDensity
+import androidx.compose.ui.unit.Dp
+import androidx.compose.ui.unit.dp
+import com.google.accompanist.swiperefresh.SwipeRefresh
+import com.google.accompanist.swiperefresh.SwipeRefreshState
+import com.google.accompanist.swiperefresh.rememberSwipeRefreshState
+import kotlinx.coroutines.delay
+
+@Composable
+fun SwipeToRefresh(content: @Composable () -> Unit) {
+    var refreshing by remember { mutableStateOf(false) }
+    LaunchedEffect(refreshing) {
+        if (refreshing) {
+            delay(2000)
+            refreshing = false
+        }
+    }
+
+
+    SwipeRefresh(
+        state = rememberSwipeRefreshState(isRefreshing = refreshing),
+        onRefresh = { refreshing = true },
+        indicator = { state, trigger ->
+            GlowIndicator(swipeRefreshState = state, refreshTriggerDistance = trigger)
+        }, content = content)
+
+
+}
+
+@Composable
+fun GlowIndicator(
+    swipeRefreshState: SwipeRefreshState,
+    refreshTriggerDistance: Dp,
+    color: Color = MaterialTheme.colors.primary,
+) {
+    Box(
+        Modifier
+            .drawWithCache {
+                onDrawBehind {
+                    val distance = refreshTriggerDistance.toPx()
+                    val progress = (swipeRefreshState.indicatorOffset / distance).coerceIn(0f, 1f)
+                    // We draw a translucent glow
+                    val brush = Brush.verticalGradient(
+                        0f to color.copy(alpha = 0.45f),
+                        1f to color.copy(alpha = 0f)
+                    )
+                    // And fade the glow in/out based on the swipe progress
+                    drawRect(brush = brush, alpha = FastOutSlowInEasing.transform(progress))
+                }
+            }
+            .fillMaxWidth()
+            .height(72.dp)
+    ) {
+        if (swipeRefreshState.isRefreshing) {
+            // If we're refreshing, show an indeterminate progress indicator
+            LinearProgressIndicator(Modifier.fillMaxWidth())
+        } else {
+            // Otherwise we display a determinate progress indicator with the current swipe progress
+            val trigger = with(LocalDensity.current) { refreshTriggerDistance.toPx() }
+            val progress = (swipeRefreshState.indicatorOffset / trigger).coerceIn(0f, 1f)
+            LinearProgressIndicator(
+                progress = progress,
+                modifier = Modifier.fillMaxWidth(),
+            )
+        }
+    }
+}
