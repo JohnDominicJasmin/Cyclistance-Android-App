e72e4e09df7f0562e4e076f81fe045292aa26484
John_Dominic <johndominicjasmin@gmail.com>
2022-05-21 21:45:13 +0800

    Adding url and use case parameter.

---
+++ app/src/main/java/com/example/cyclistance/common/MappingConstants.kt
@@ -0,0 +1,5 @@
+package com.example.cyclistance.common
+
+object MappingConstants {
+    const val CYCLISTANCE_API_BASE_URL = "https://cyclistance-app.herokuapp.com/"
+}
--- app/src/main/java/com/example/cyclistance/feature_main_screen/presentation/mapping_main_screen/MappingViewModel.kt
+++ app/src/main/java/com/example/cyclistance/feature_main_screen/presentation/mapping_main_screen/MappingViewModel.kt
@@ -1,14 +1,20 @@
 package com.example.cyclistance.feature_main_screen.presentation.mapping_main_screen
 
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
 import com.example.cyclistance.feature_authentication.domain.use_case.AuthenticationUseCase
+import com.example.cyclistance.feature_main_screen.domain.use_case.MappingUseCase
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.flow.launchIn
+import kotlinx.coroutines.flow.onEach
+import timber.log.Timber
 
 import javax.inject.Inject
 
 @HiltViewModel
 class MappingViewModel @Inject constructor(
-    private val authUseCase: AuthenticationUseCase):ViewModel() {
+    private val authUseCase: AuthenticationUseCase,
+    private val mappingUseCase: MappingUseCase):ViewModel() {
 
 
     fun getEmail():String = authUseCase.getEmailUseCase() ?: "--------"
