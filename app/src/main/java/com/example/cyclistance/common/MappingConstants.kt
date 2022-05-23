ba955b2c5c58b8ca7b043bfd67b790a662da2f59
John_Dominic <johndominicjasmin@gmail.com>
2022-05-24 02:35:52 +0800

    Add Constants values.

--- app/src/main/java/com/example/cyclistance/common/MappingConstants.kt
+++ app/src/main/java/com/example/cyclistance/common/MappingConstants.kt
@@ -2,4 +2,12 @@ package com.example.cyclistance.common
 
 object MappingConstants {
     const val CYCLISTANCE_API_BASE_URL = "https://cyclistance-app.herokuapp.com/"
+    const val MAX_ZOOM_LEVEL_MAPS = 18.85
+    const val MIN_ZOOM_LEVEL_MAPS = 3.00
+
+    const val CAMERA_TILT_DEGREES = 30.00
+    const val DEFAULT_LATITUDE = 12.8797
+    const val DEFAULT_LONGITUDE = 121.7740
+    const val DEFAULT_CAMERA_ANIMATION_DURATION = 700
+    const val MAP_ZOOM = 6.00
 }
