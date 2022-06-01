2fd12998ca05c58ec84778ee1c8474bb2faf25c4
John_Dominic <johndominicjasmin@gmail.com>
2022-06-02 00:35:06 +0800

    Change to proper color naming convention.

--- app/src/main/java/com/example/cyclistance/theme/Color.kt
+++ app/src/main/java/com/example/cyclistance/theme/Color.kt
@@ -2,29 +2,6 @@ package com.example.cyclistance.theme
 
 import androidx.compose.ui.graphics.Color
 
-/*
-val BackgroundColor = Color(0xFF303234)
-val TextFieldBackgroundColor = Color(0xFF545454)
-val ThemeColor = Color(0xFFEBAD52)
-val GoogleButtonTextColor = Color(0xFF8C8C8C)
-//val TextFieldTextHintColor = Color(0xFFC4C4C4)
-val FacebookColor = Color(0xFF3B5998)
-val DisabledColor = Color(0xFF9F9E9D)
-val TopAppBarBackgroundColor = Color(0xFF3B3B3B)
-val ShowableDisplaysBackgroundColor = Color(0xFF545454)
-val ShowableDisplaysBorderColor = Color(0xFFA6A6A6)
-val RoundedButtonBackgroundColor = Color(0xFF222222)
-val RoundedButtonContentColor = Color(0xFFB8B8B8)
-val RoundedButtonSubtitleColor = Color(0xFFA5A5A5)
-val ChangeProfileTextColor = Color(0xFF778DFF)
-val EditProfileTextFieldIndicatorColor = Color(0xFF959595)
-val ErrorColor = Color(0xFFFF746B)
-val MessageTextFieldBackground = Color(0xFF404040)
-
-*/
-
-
-//919191
 
 val Black900 = Color(0xFF303234)//background color
 val Black850 = Color(0xFF5A5A5A)//textfield color secondary
@@ -36,6 +13,8 @@ val Black440 = Color(0xFF979797)
 val Orange300 = Color(0xFFEBAD52)
 val Orange30 = Color(0x4DD6B27B)
 
-val _Red600 = Color(0xFFFF746B)
+val Red600 = Color(0xFFFF746B)
 
+
+val Blue600 = Color(0xFF5786FF)
 val Blue500 = Color(0xFF3B5998)
