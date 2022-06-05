2caaf2830893dd6cecc7055f0265cd7d951dd601
John_Dominic <johndominicjasmin@gmail.com>
2022-06-05 20:11:37 +0800

    Add color for light theme.

--- app/src/main/java/com/example/cyclistance/theme/Color.kt
+++ app/src/main/java/com/example/cyclistance/theme/Color.kt
@@ -2,14 +2,15 @@ package com.example.cyclistance.theme
 
 import androidx.compose.ui.graphics.Color
 
-
+val Black910 = Color(0xFF313131)
 val Black900 = Color(0xFF303234)
 val Black850 = Color(0xFF5A5A5A)
 val Black800 = Color(0xFF484848)
-val Black500 = Color(0xFFA5A5A5)
+val Black500 = Color(0xFFA5A5A5)//should be use for textFields
 val Black450 = Color(0xFF8C8C8C)
-val Black440 = Color(0xFF979797)
-val Black300 = Color(0xFFDDDDDD)
+val Black445 = Color(0xFF4D4D4D)
+val Black440 = Color(0xFF979797)// should be use for surfaces
+val Black300 = Color(0xFF919191)
 
 
 val Orange300 = Color(0xFFEBAD52)
@@ -22,4 +23,7 @@ val Blue600 = Color(0xFF5786FF)
 val Blue500 = Color(0xFF3B5998)
 
 
+val White200 = Color(0xFFEDEDED)
+val White100 = Color(0xFFF2F2F2)
+val White50 = Color(0xFFFDFDFD)
 
