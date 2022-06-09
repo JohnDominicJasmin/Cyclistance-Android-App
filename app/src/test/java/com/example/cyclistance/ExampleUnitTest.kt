1dfd0598b32c9a1d32d0e1c913f10f989abadaa0
John_Dominic <johndominicjasmin@gmail.com>
2022-06-09 23:12:25 +0800

    Write unit test.

--- app/src/test/java/com/example/cyclistance/ExampleUnitTest.kt
+++ app/src/test/java/com/example/cyclistance/ExampleUnitTest.kt
@@ -2,8 +2,6 @@ package com.example.cyclistance
 
 import org.junit.Test
 
-import org.junit.Assert.*
-
 /**
  * Example local unit test, which will execute on the development machine (host).
  *
@@ -11,9 +9,28 @@ import org.junit.Assert.*
  */
 class ExampleUnitTest {
     @Test
-    fun addition_isCorrect() {
+    fun resizeImage() {
         val sample = "https://lh3.googleusercontent.com/a-/AOh14GhTD_mkobyJg2FxApjwLul1sgaEfuKHlPr-rVsV=s96-c"
         val res = sample.replace(oldValue = "=s96-c", newValue = "=s220-c");
         print("$res \n \n")
+    }
+
+    @Test
+    fun filterName(){
+        assert(value = getName()=="johndoe394").also{
+            print("Result is ${getName()}")
+        }
+    }
+    private fun getNameUseCase():String?{
+        return null
+    }
+    private fun getName()=
+        getNameUseCase()?:getEmail().apply{
+            val index = this.indexOf('@')
+            return this.substring(0, index)
+        }
+
+    private fun getEmail():String {
+        return "johndoe394@gmail.com"
     }
 }
