faa68f7125bf2714d897686b641d7bf1673a6184
John_Dominic <johndominicjasmin@gmail.com>
2022-06-24 00:37:24 +0800

    Change Row to Button to make it more accessible.

--- app/src/main/java/com/example/cyclistance/feature_settings/presentation/setting_main_screen/components/SettingsButtonItem.kt
+++ app/src/main/java/com/example/cyclistance/feature_settings/presentation/setting_main_screen/components/SettingsButtonItem.kt
@@ -1,47 +1,46 @@
 package com.example.cyclistance.feature_settings.presentation.setting_main_screen.components
 
-import androidx.compose.foundation.background
-import androidx.compose.foundation.clickable
-import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.wrapContentSize
-import androidx.compose.material.Icon
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Text
+import androidx.compose.material.*
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.example.cyclistance.R
+import com.example.cyclistance.theme.CyclistanceTheme
 
 
 @Composable
-fun ButtonItem(buttonText: String) {
-    Row(
+fun SettingsButtonItem(buttonText: String, onClick: () -> Unit) {
 
-        modifier = Modifier
-            .fillMaxWidth()
-            .background(MaterialTheme.colors.background)
-            .padding(start = 3.dp)
-            .wrapContentSize()
-            .clickable {
+    SettingsButtonCreator(onClick = onClick) {
 
-            },
-        verticalAlignment = Alignment.CenterVertically) {
-
-        Text(text = buttonText,
+        Text(
+            text = buttonText,
             color = MaterialTheme.colors.onBackground,
             modifier = Modifier
                 .weight(0.88f)
-                .padding(top = 7.dp, bottom = 7.dp))
+                .padding(top = 7.dp, bottom = 7.dp, start = 5.dp))
         Icon(
             painter = painterResource(id = R.drawable.ic_arrow),
             contentDescription = "Arrow",
             tint = Color.Unspecified)
 
+
     }
 }
 
+
+
+@Preview
+@Composable
+fun ButtonItemPreview() {
+
+    CyclistanceTheme(true) {
+        SettingsButtonItem(buttonText = "Edit Profile") {
+
+        }
+    }
+}
