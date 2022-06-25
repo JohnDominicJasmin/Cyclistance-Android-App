a692fcb48338bd7ecceff2775a1931033347ef4b
John_Dominic <johndominicjasmin@gmail.com>
2022-06-25 22:48:20 +0800

    Remove surface background color and fix image changing into oval shape.

--- app/src/main/java/com/example/cyclistance/feature_settings/presentation/setting_edit_profile/components/EditProfilePictureArea.kt
+++ app/src/main/java/com/example/cyclistance/feature_settings/presentation/setting_edit_profile/components/EditProfilePictureArea.kt
@@ -2,40 +2,47 @@ package com.example.cyclistance.feature_settings.presentation.setting_edit_profi
 
 import android.graphics.Bitmap
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
+import androidx.compose.ui.draw.shadow
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.ImageBitmap
 import androidx.compose.ui.graphics.asImageBitmap
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import coil.compose.AsyncImage
 import com.example.cyclistance.R
+import com.example.cyclistance.common.MappingConstants
+import com.example.cyclistance.theme.CyclistanceTheme
 
 
 @Composable
 fun ProfilePictureArea(photoUrl: Any, modifier: Modifier, onClick: () -> Unit) {
 
-
-    Surface(modifier = modifier
-        .clip(CircleShape)) {
+    Surface(
+        modifier = modifier,
+        color = (Color.Transparent),
+        shape = CircleShape,
+    ) {
 
         Column(
-            verticalArrangement = Arrangement.spacedBy(
-                6.5.dp,
-                alignment = Alignment.CenterVertically),
+            verticalArrangement = Arrangement.spacedBy(6.5.dp, alignment = Alignment.CenterVertically),
             horizontalAlignment = Alignment.CenterHorizontally) {
 
             Box(modifier = Modifier.padding(all = 10.dp)) {
 
 
-                when(photoUrl){
+                when (photoUrl) {
 
                     is String -> {
                         AsyncImage(
@@ -44,6 +51,7 @@ fun ProfilePictureArea(photoUrl: Any, modifier: Modifier, onClick: () -> Unit) {
                             contentDescription = "User Profile Image",
                             modifier = Modifier
                                 .clip(CircleShape)
+                                .fillMaxSize()
                                 .clickable { onClick() }
                                 .align(Alignment.Center),
                             contentScale = ContentScale.Crop,
@@ -59,6 +67,7 @@ fun ProfilePictureArea(photoUrl: Any, modifier: Modifier, onClick: () -> Unit) {
                           alignment = Alignment.Center,
                           modifier = Modifier
                               .clip(CircleShape)
+                              .fillMaxSize()
                               .clickable { onClick() }
                               .align(Alignment.Center),
                           contentScale = ContentScale.Crop,
@@ -76,6 +85,23 @@ fun ProfilePictureArea(photoUrl: Any, modifier: Modifier, onClick: () -> Unit) {
                     contentScale = ContentScale.Crop)
             }
 
+        }
+    }
+}
+
+@Preview
+@Composable
+fun ProfilePictureAreaPreview() {
+
+    CyclistanceTheme(false) {
+        Column(modifier = Modifier
+            .fillMaxSize()
+            .background(MaterialTheme.colors.background)) {
+
+            ProfilePictureArea(
+                photoUrl = MappingConstants.IMAGE_PLACEHOLDER_URL,
+                modifier = Modifier.size(125.dp),
+                onClick = {})
         }
     }
 }
