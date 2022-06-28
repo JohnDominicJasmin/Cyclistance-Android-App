d6433afa92edd0ee9f0e5db91c2272b620ec6676
John_Dominic <johndominicjasmin@gmail.com>
2022-06-29 02:46:00 +0800

    Add bottom sheet to select  image from gallery or camera.

--- app/src/main/java/com/example/cyclistance/feature_settings/presentation/setting_edit_profile/components/EditProfileBottomSheets.kt
+++ app/src/main/java/com/example/cyclistance/feature_settings/presentation/setting_edit_profile/components/EditProfileBottomSheets.kt
@@ -1,20 +1,120 @@
 package com.example.cyclistance.feature_settings.presentation.setting_edit_profile.components
 
-import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.material.MaterialTheme
+import androidx.annotation.DrawableRes
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.*
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.constraintlayout.compose.ConstraintLayout
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import kotlinx.coroutines.launch
+import com.example.cyclistance.R
+
+@ExperimentalMaterialApi
+@Composable
+fun SelectImageBottomSheet(
+    galleryButtonOnClick: () -> Unit,
+    cameraButtonOnClick: () -> Unit,
+    bottomSheetScaffoldState: ModalBottomSheetState,
+    content: @Composable () -> Unit) {
+
+
+    ModalBottomSheetLayout(
+        sheetState = bottomSheetScaffoldState,
+        sheetContent = {
+            Card(
+                modifier = Modifier.fillMaxWidth()) {
+
+
+                Column(
+                    modifier = Modifier
+                        .padding(top = 4.dp, bottom = 4.dp)) {
+
+                    BottomSheetButtonItem(
+                        iconId = R.drawable.ic_gallery,
+                        buttonText = "Open Gallery",
+                        onClick = galleryButtonOnClick)
+
+                    BottomSheetButtonItem(
+                        iconId = R.drawable.ic_camera,
+                        buttonText = "Take Photo",
+                        onClick = cameraButtonOnClick)
+                }
+
+            }
+        },
+        sheetShape = RoundedCornerShape(topStart = 12.dp, topEnd = 12.dp),
+        sheetElevation = 12.dp,
+        sheetBackgroundColor = MaterialTheme.colors.surface,
+        sheetContentColor = MaterialTheme.colors.onSurface,
+        scrimColor = ModalBottomSheetDefaults.scrimColor.copy(alpha = 0.10f),
+        content = content
+    )
+
+
+}
+
 
 @Composable
-fun EditProfileSelectImage() {
-    ConstraintLayout(  modifier = Modifier
-        .fillMaxWidth()
-        .background(color = MaterialTheme.colors.surface)) {
+fun BottomSheetButtonItem(@DrawableRes iconId: Int, buttonText: String, onClick: () -> Unit) {
 
+    TextButton(
+        onClick = onClick,
+        modifier = Modifier.fillMaxWidth(),
+        elevation = ButtonDefaults.elevation(
+            defaultElevation = 0.dp,
+            pressedElevation = 0.dp)) {
 
+        Row(
+            horizontalArrangement = Arrangement.spacedBy(4.dp, alignment = Alignment.Start),
+            verticalAlignment = Alignment.CenterVertically,
+            modifier = Modifier
+                .weight(0.3f)
+                .padding(start = 8.5.dp)) {
+            Icon(
+                painter = painterResource(id = iconId),
+                contentDescription = "$buttonText Icon",
+                tint = MaterialTheme.colors.onSurface,
+                modifier = Modifier.padding(start = 3.dp, end = 12.dp))
 
+            Text(
+                text = buttonText,
+                color = MaterialTheme.colors.onSurface,
+                style = MaterialTheme.typography.button,
 
+                )
+        }
+    }
+
+}
+
+@ExperimentalMaterialApi
+@Preview(showSystemUi = true, showBackground = true)
+@Composable
+fun BottomSheetPreview() {
+    val bottomSheetScaffoldState = rememberModalBottomSheetState(ModalBottomSheetValue.Hidden)
+
+    val scope = rememberCoroutineScope()
+
+    SelectImageBottomSheet(
+        galleryButtonOnClick = {},
+        cameraButtonOnClick = {},
+        bottomSheetScaffoldState = bottomSheetScaffoldState) {
+        Button(onClick = {
+            scope.launch {
+
+                if (bottomSheetScaffoldState.isVisible) {
+                    bottomSheetScaffoldState.hide()
+                } else {
+                    bottomSheetScaffoldState.show()
+                }
+            }
+        }) {
+            Text(text = "Expand/Collapse Bottom Sheet")
+        }
     }
 }
