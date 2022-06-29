d7c0688116f525949d1f588897c382e2ecb171fc
John_Dominic <johndominicjasmin@gmail.com>
2022-06-30 01:30:10 +0800

    Add bottom sheet selecting open gallery or take photo.

--- app/src/main/java/com/example/cyclistance/feature_settings/presentation/setting_edit_profile/components/EditProfileScreen.kt
+++ app/src/main/java/com/example/cyclistance/feature_settings/presentation/setting_edit_profile/components/EditProfileScreen.kt
@@ -10,11 +10,10 @@ import android.widget.Toast
 import androidx.activity.compose.rememberLauncherForActivityResult
 import androidx.activity.result.IntentSenderRequest
 import androidx.activity.result.contract.ActivityResultContracts
+import androidx.activity.result.launch
 import androidx.compose.foundation.*
 import androidx.compose.foundation.layout.*
-import androidx.compose.material.CircularProgressIndicator
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Text
+import androidx.compose.material.*
 import androidx.compose.runtime.*
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.asImageBitmap
@@ -32,17 +31,18 @@ import com.example.cyclistance.feature_settings.presentation.setting_edit_profil
 import com.example.cyclistance.feature_settings.presentation.setting_edit_profile.EditProfileUiEvent
 import com.example.cyclistance.feature_settings.presentation.setting_edit_profile.EditProfileViewModel
 import com.example.cyclistance.theme.*
-import com.google.accompanist.permissions.ExperimentalPermissionsApi
-import com.google.accompanist.permissions.isGranted
-import com.google.accompanist.permissions.rememberPermissionState
-import com.google.accompanist.permissions.shouldShowRationale
+import com.google.accompanist.permissions.*
 import com.google.android.gms.auth.api.credentials.Credential
 import com.google.android.gms.auth.api.credentials.Credentials
 import com.google.android.gms.auth.api.credentials.HintRequest
 import kotlinx.coroutines.flow.collectLatest
+import kotlinx.coroutines.launch
 import timber.log.Timber
+import java.io.ByteArrayOutputStream
+import java.io.File
 
-@OptIn(ExperimentalPermissionsApi::class)
+
+@OptIn(ExperimentalPermissionsApi::class, ExperimentalMaterialApi::class)
 @Composable
 fun EditProfileScreen(
     editProfileViewModel: EditProfileViewModel = hiltViewModel(),
@@ -50,6 +50,18 @@ fun EditProfileScreen(
     navigateTo: (destination: String, popUpToDestination: String?) -> Unit) {
 
     val state by editProfileViewModel.state
+
+    val bottomSheetState = rememberModalBottomSheetState(ModalBottomSheetValue.Hidden)
+    val scope = rememberCoroutineScope()
+
+    val toggleBottomSheet = {
+        scope.launch {
+            with(bottomSheetState) {
+               if(isVisible) hide() else show()
+            }
+        }
+    }
+
 
     val context = LocalContext.current
     val hintRequest = HintRequest.Builder()
@@ -60,12 +72,12 @@ fun EditProfileScreen(
     val intentSender = IntentSenderRequest.Builder(simCardIntent.intentSender).build()
 
     val simCardResultLauncher = rememberLauncherForActivityResult(
-        ActivityResultContracts.StartIntentSenderForResult()
-    ) { result ->
+        ActivityResultContracts.StartIntentSenderForResult()) { result ->
 
         when (result.resultCode) {
             Activity.RESULT_OK -> {
-                val credential: Credential? = result.data?.getParcelableExtra(Credential.EXTRA_KEY)
+                val credential: Credential? =
+                    result.data?.getParcelableExtra(Credential.EXTRA_KEY)
                 editProfileViewModel.onEvent(
                     event = EditProfileEvent.EnteredPhoneNumber(
                         phoneNumber = TextFieldValue(
@@ -77,14 +89,14 @@ fun EditProfileScreen(
             NONE_OF_THE_ABOVE_RESULT_CODE -> {
                 editProfileViewModel.onEvent(
                     event = EditProfileEvent.EnteredPhoneNumber(
-                        phoneNumber = TextFieldValue(
-                            text = "")))
+                        phoneNumber = TextFieldValue(text = "")))
             }
         }
     }
-    LaunchedEffect(true){
+
+    LaunchedEffect(true) {
         editProfileViewModel.eventFlow.collectLatest { event ->
-            when(event){
+            when (event) {
                 is EditProfileUiEvent.ShowMappingScreen -> {
                     onPopBackStack()
                 }
@@ -92,7 +104,6 @@ fun EditProfileScreen(
             }
         }
     }
-
     val openGalleryResultLauncher =
         rememberLauncherForActivityResult(ActivityResultContracts.GetContent()) { uri: Uri? ->
             editProfileViewModel.onEvent(event = EditProfileEvent.NewImageUri(uri = uri))
@@ -107,7 +118,9 @@ fun EditProfileScreen(
                             }
                             else -> {
                                 val source =
-                                    ImageDecoder.createSource(context.contentResolver, selectedUri)
+                                    ImageDecoder.createSource(
+                                        context.contentResolver,
+                                        selectedUri)
                                 ImageDecoder.decodeBitmap(source)
                             }
                         }
@@ -115,8 +128,16 @@ fun EditProfileScreen(
             }
         }
 
+    val openCameraResultLauncher =
+        rememberLauncherForActivityResult(ActivityResultContracts.TakePicturePreview()) { bitmap ->
 
-    val permissionState =
+            editProfileViewModel.onEvent(event = EditProfileEvent.NewBitmapPicture(bitmap = bitmap))
+            editProfileViewModel.onEvent(event = EditProfileEvent.SaveImageToGallery)
+
+        }
+
+
+    val openGalleryPermissionState =
         rememberPermissionState(permission = Manifest.permission.READ_EXTERNAL_STORAGE) { permissionGranted ->
             if (permissionGranted) {
                 openGalleryResultLauncher.launch("image/*")
@@ -124,88 +145,116 @@ fun EditProfileScreen(
         }
 
 
+    val openCameraPermissionState =
+        rememberMultiplePermissionsState(permissions = listOf(Manifest.permission.CAMERA, Manifest.permission.WRITE_EXTERNAL_STORAGE)) { permissionGranted ->
 
-    ConstraintLayout(
-        modifier = Modifier
-            .fillMaxSize()
-            .background(MaterialTheme.colors.background)) {
+        if (permissionGranted.values.all { it }) {
+            openCameraResultLauncher.launch()
+        }
 
 
-        val (profilePictureArea, textFieldInputArea, buttonNavigationArea, changePhotoText, progressBar) = createRefs()
+    }
 
-        ProfilePictureArea(
-            photoUrl = state.bitmap?.asImageBitmap() ?: state.photoUrl,
+
+    SelectImageBottomSheet(
+        galleryButtonOnClick = {
+            toggleBottomSheet()
+            if (openGalleryPermissionState.status.isGranted) {
+                openGalleryResultLauncher.launch("image/*")
+                return@SelectImageBottomSheet
+            }
+            if (!openGalleryPermissionState.status.shouldShowRationale) {
+                Toast.makeText(context, "Permission to open Gallery is denied.", Toast.LENGTH_LONG).show()
+                return@SelectImageBottomSheet
+            }
+            openGalleryPermissionState.launchPermissionRequest()
+        },
+        cameraButtonOnClick = {
+            toggleBottomSheet()
+            if(openCameraPermissionState.allPermissionsGranted){
+                openCameraResultLauncher.launch()
+                return@SelectImageBottomSheet
+            }
+            if(!openCameraPermissionState.shouldShowRationale){
+                Toast.makeText(context, "Permissions to open Camera is denied.",Toast.LENGTH_LONG).show()
+                return@SelectImageBottomSheet
+            }
+            openCameraPermissionState.launchMultiplePermissionRequest()
+        },
+        bottomSheetScaffoldState = bottomSheetState) {
+
+        ConstraintLayout(
             modifier = Modifier
-                .size(125.dp)
-                .constrainAs(profilePictureArea) {
+                .fillMaxSize()
+                .background(MaterialTheme.colors.background)) {
 
-                    top.linkTo(parent.top, margin = 30.dp)
+
+            val (profilePictureArea, textFieldInputArea, buttonNavigationArea, changePhotoText, progressBar) = createRefs()
+
+            ProfilePictureArea(
+                photoUrl = state.bitmap?.asImageBitmap() ?: state.photoUrl,
+                modifier = Modifier
+                    .size(125.dp)
+                    .constrainAs(profilePictureArea) {
+
+                        top.linkTo(parent.top, margin = 30.dp)
+                        start.linkTo(parent.start)
+                        end.linkTo(parent.end)
+
+                    },
+                onClick = {
+                    toggleBottomSheet()
+                })
+
+
+
+
+            Text(
+                text = "Change Profile Photo",
+                color = Blue600,
+                style = MaterialTheme.typography.caption, fontWeight = FontWeight.SemiBold,
+                modifier = Modifier.constrainAs(changePhotoText) {
+                    top.linkTo(profilePictureArea.bottom, margin = 12.dp)
                     start.linkTo(parent.start)
                     end.linkTo(parent.end)
 
+                })
+
+
+            TextFieldInputArea(modifier = Modifier
+                .constrainAs(textFieldInputArea) {
+                    top.linkTo(changePhotoText.bottom, margin = 30.dp)
+                    start.linkTo(parent.start)
+                    end.linkTo(parent.end)
+                    height = Dimension.percent(0.5f)
+                    width = Dimension.percent(0.9f)
+
                 },
-            onClick = {
-                if (permissionState.status.isGranted) {
-                    openGalleryResultLauncher.launch("image/*")
-                    return@ProfilePictureArea
-                }
-                if (!permissionState.status.shouldShowRationale) {
-                    Toast.makeText(context, "Permission is denied.", Toast.LENGTH_SHORT).show()
-                    return@ProfilePictureArea
-                }
-
-                permissionState.launchPermissionRequest()
-            })
+                editProfileViewModel = editProfileViewModel,
+                onPhoneTextFieldClick = {
+                    simCardResultLauncher.launch(intentSender)
+                })
 
 
+            MappingButtonNavigation(modifier = Modifier
+                .constrainAs(buttonNavigationArea) {
+                    top.linkTo(textFieldInputArea.bottom, margin = 20.dp)
+                    start.linkTo(parent.start)
+                    end.linkTo(parent.end)
+                    bottom.linkTo(parent.bottom, margin = 50.dp)
+                    height = Dimension.percent(0.1f)
+                    width = Dimension.percent(0.8f)
+                },
+                positiveButtonText = "Save",
+                onClickCancelButton = {
+                    onPopBackStack()
+                },
+                onClickConfirmButton = {
+                    editProfileViewModel.onEvent(event = EditProfileEvent.Save)
+                })
 
-
-        Text(
-            text = "Change Profile Photo",
-            color = Blue600,
-            style = MaterialTheme.typography.caption, fontWeight = FontWeight.SemiBold,
-            modifier = Modifier.constrainAs(changePhotoText) {
-                top.linkTo(profilePictureArea.bottom, margin = 12.dp)
-                start.linkTo(parent.start)
-                end.linkTo(parent.end)
-
-            })
-
-
-        TextFieldInputArea(modifier = Modifier
-            .constrainAs(textFieldInputArea) {
-                top.linkTo(changePhotoText.bottom, margin = 30.dp)
-                start.linkTo(parent.start)
-                end.linkTo(parent.end)
-                height = Dimension.percent(0.5f)
-                width = Dimension.percent(0.9f)
-
-            },
-            editProfileViewModel = editProfileViewModel,
-            onPhoneTextFieldClick = {
-                simCardResultLauncher.launch(intentSender)
-            })
-
-
-        MappingButtonNavigation(modifier = Modifier
-            .constrainAs(buttonNavigationArea) {
-                top.linkTo(textFieldInputArea.bottom, margin = 20.dp)
-                start.linkTo(parent.start)
-                end.linkTo(parent.end)
-                bottom.linkTo(parent.bottom, margin = 50.dp)
-                height = Dimension.percent(0.1f)
-                width = Dimension.percent(0.8f)
-            },
-            positiveButtonText = "Save",
-            onClickCancelButton = {
-                onPopBackStack()
-            },
-            onClickConfirmButton = {
-                editProfileViewModel.onEvent(event = EditProfileEvent.Save)
-            })
-
-            if(state.isLoading){
-                CircularProgressIndicator(modifier = Modifier.constrainAs(progressBar){
+            if (state.isLoading) {
+                CircularProgressIndicator(modifier = Modifier.constrainAs(progressBar) {
                     top.linkTo(parent.top)
                     end.linkTo(parent.end)
                     start.linkTo(parent.start)
@@ -213,6 +262,9 @@ fun EditProfileScreen(
                     this.centerTo(parent)
                 })
             }
+        }
     }
 }
+
+
 
