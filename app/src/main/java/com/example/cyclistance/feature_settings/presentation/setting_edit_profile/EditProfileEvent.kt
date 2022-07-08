4d760a29312fb7c1fa50f9ddab1acb64d65606d8
John_Dominic <johndominicjasmin@gmail.com>
2022-07-09 03:59:21 +0800

    Change class name.

--- app/src/main/java/com/example/cyclistance/feature_settings/presentation/setting_edit_profile/EditProfileEvent.kt
+++ app/src/main/java/com/example/cyclistance/feature_settings/presentation/setting_edit_profile/EditProfileEvent.kt
@@ -12,8 +12,8 @@ sealed class EditProfileEvent {
     object LoadPhoto: EditProfileEvent()
     object LoadName: EditProfileEvent()
     object LoadPhoneNumber: EditProfileEvent()
-    data class NewImageUri(val uri: Uri?): EditProfileEvent()
-    data class NewBitmapPicture(val bitmap: Bitmap?): EditProfileEvent()
+    data class SelectImageUri(val uri: Uri?): EditProfileEvent()
+    data class SelectBitmapPicture(val bitmap: Bitmap?): EditProfileEvent()
     object SaveImageToGallery: EditProfileEvent()
 
 }
