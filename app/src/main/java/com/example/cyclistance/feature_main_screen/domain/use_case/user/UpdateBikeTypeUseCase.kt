1816c9109cef702e1b26d17ec04909da042276b1
John_Dominic <johndominicjasmin@gmail.com>
2022-08-29 02:04:52 +0800

    Create UseCase to Get and Update bike type.

---
+++ app/src/main/java/com/example/cyclistance/feature_main_screen/domain/use_case/user/UpdateBikeTypeUseCase.kt
@@ -0,0 +1,14 @@
+package com.example.cyclistance.feature_main_screen.domain.use_case.user
+
+import com.example.cyclistance.feature_main_screen.domain.exceptions.MappingExceptions
+import com.example.cyclistance.feature_main_screen.domain.repository.MappingRepository
+
+class UpdateBikeTypeUseCase(private val repository: MappingRepository) {
+    suspend operator fun invoke(value: String?){
+
+        if(value!!.isEmpty()){
+            throw MappingExceptions.BikeTypeException()
+        }
+        repository.updatePreference(value)
+    }
+}
