baadc33ffef9c82f5fda456d2784913ba509845b
John_Dominic <johndominicjasmin@gmail.com>
2022-08-29 02:04:52 +0800

    Create UseCase to Get and Update bike type.

---
+++ app/src/main/java/com/example/cyclistance/feature_main_screen/domain/use_case/user/GetBikeTypeUseCase.kt
@@ -0,0 +1,13 @@
+package com.example.cyclistance.feature_main_screen.domain.use_case.user
+
+import com.example.cyclistance.feature_main_screen.domain.repository.MappingRepository
+import kotlinx.coroutines.flow.Flow
+
+
+class GetBikeTypeUseCase(private val repository: MappingRepository) {
+
+    operator fun invoke(): Flow<String?> {
+        return repository.getPreference()
+    }
+
+}
