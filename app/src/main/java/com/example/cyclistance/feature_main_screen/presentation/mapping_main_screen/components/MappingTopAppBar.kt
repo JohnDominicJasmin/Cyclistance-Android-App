61c33918e7ec8966d2fdee32459d3feece8fc471
John_Dominic <johndominicjasmin@gmail.com>
2022-09-02 03:06:29 +0800

    Fix TopAppBar not showing.

--- app/src/main/java/com/example/cyclistance/feature_main_screen/presentation/mapping_main_screen/components/MappingTopAppBar.kt
+++ app/src/main/java/com/example/cyclistance/feature_main_screen/presentation/mapping_main_screen/components/MappingTopAppBar.kt
@@ -3,13 +3,15 @@ package com.example.cyclistance.feature_main_screen.presentation.mapping_main_sc
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.*
 import androidx.compose.material.*
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Menu
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.vector.ImageVector
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.example.cyclistance.R
@@ -23,11 +25,14 @@ fun TopAppBarCreator(
 
     TopAppBar(
         elevation = 10.dp,
-        title = { topAppBarTitle() },
+        title = topAppBarTitle,
         backgroundColor = MaterialTheme.colors.background,
         navigationIcon = {
             IconButton(onClick = onClickIcon) {
-                Icon(icon, contentDescription = "", tint = MaterialTheme.colors.onBackground)
+                Icon(
+                    icon,
+                    contentDescription = "Top App Bar Icon",
+                    tint = MaterialTheme.colors.onBackground)
             }
         })
 
@@ -35,42 +40,63 @@ fun TopAppBarCreator(
 
 
 @Composable
-fun DefaultTitleTopAppBar() {
-
-    Row(
-        modifier = Modifier.wrapContentSize(),
-        verticalAlignment = Alignment.CenterVertically,
-        horizontalArrangement = Arrangement.spacedBy(5.dp)) {
-
-        Image(
-            painter = painterResource(id = R.drawable.ic_app_icon_cyclistance),
-            contentDescription = "Image Icon",
-            alignment = Alignment.Center,
-            modifier = Modifier
-                .size(30.dp)
-                .align(alignment = Alignment.CenterVertically)
-                .padding(top = 5.dp)
-        )
+fun TitleTopAppBar(
+    title: String,
+    confirmationText: String? = null,
+    onConfirmationClick: () -> Unit = {}) {
+    Row(verticalAlignment = Alignment.CenterVertically) {
         Text(
-            "Cyclistance",
+            text = title,
             color = MaterialTheme.colors.onBackground,
             fontWeight = FontWeight.Medium,
             fontSize = 20.sp)
-    }
 
+        Spacer(modifier = Modifier.weight(0.1f))
+        confirmationText?.let {
+            TextButton(onClick = onConfirmationClick, modifier = Modifier.padding(end = 4.dp)) {
+                Text(
+                    text = "SAVE",
+                    color = MaterialTheme.colors.onBackground,
+                    fontWeight = FontWeight.Medium,
+                    fontSize = 14.sp)
+            }
+        }
+    }
 }
 
+@Preview
 @Composable
-fun DetailsTitleTopAppBar(text: String) {
-    Row(
-        modifier = Modifier.wrapContentSize(),
-        verticalAlignment = Alignment.CenterVertically,
-        horizontalArrangement = Arrangement.spacedBy(5.dp)) {
+fun TopAppBarPreview() {
+    TitleTopAppBar(title = "Title")
+}
 
-        Text(
-            text = text,
-            color = MaterialTheme.colors.onBackground,
-            fontWeight = FontWeight.Medium,
-            fontSize = 20.sp)
-    }
+
+@Composable
+fun DefaultTopBar(onClickIcon: () -> Unit) {
+    TopAppBarCreator(
+        icon = Icons.Filled.Menu,
+        onClickIcon = onClickIcon,
+        topAppBarTitle = {
+            Row(
+                modifier = Modifier.wrapContentSize(),
+                verticalAlignment = Alignment.CenterVertically,
+                horizontalArrangement = Arrangement.spacedBy(5.dp)) {
+
+                Image(
+                    painter = painterResource(id = R.drawable.ic_app_icon_cyclistance),
+                    contentDescription = "Image Icon",
+                    alignment = Alignment.Center,
+                    modifier = Modifier
+                        .size(30.dp)
+                        .align(alignment = Alignment.CenterVertically)
+                        .padding(top = 5.dp)
+                )
+                Text(
+                    "Cyclistance",
+                    color = MaterialTheme.colors.onBackground,
+                    fontWeight = FontWeight.Medium,
+                    fontSize = 20.sp)
+            }
+        })
+
 }
