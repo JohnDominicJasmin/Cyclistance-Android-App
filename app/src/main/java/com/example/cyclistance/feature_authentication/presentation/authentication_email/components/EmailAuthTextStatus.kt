9f675e3db52cc0563e1e8e5f191acd864bd7e782
John_Dominic <johndominicjasmin@gmail.com>
2022-09-04 21:39:18 +0800

    Remove redundant text.

--- app/src/main/java/com/example/cyclistance/feature_authentication/presentation/authentication_email/components/EmailAuthTextStatus.kt
+++ app/src/main/java/com/example/cyclistance/feature_authentication/presentation/authentication_email/components/EmailAuthTextStatus.kt
@@ -7,20 +7,16 @@ import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.layoutId
-import androidx.compose.ui.text.SpanStyle
-import androidx.compose.ui.text.buildAnnotatedString
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
-import androidx.compose.ui.text.withStyle
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.example.cyclistance.feature_authentication.presentation.common.AuthenticationConstraintsItem
 
 @Composable
-fun EmailAuthTextStatus(email:String) {
+fun EmailAuthTextStatus(email: String) {
 
 
     Column(
@@ -28,40 +24,31 @@ fun EmailAuthTextStatus(email:String) {
         horizontalAlignment = Alignment.CenterHorizontally,
         verticalArrangement = Arrangement.spacedBy(10.dp)) {
 
+        EmailStatus(text = "Confirm your email address", fontWeight = FontWeight.ExtraBold)
 
-        Text(
-            text = "Confirm your email address",
-            color = MaterialTheme.colors.onBackground,
-            fontSize = 16.sp,
-            fontWeight = FontWeight.ExtraBold,
-            textAlign = TextAlign.Center)
+        EmailStatus(text = "We sent confirmation email to:", fontWeight = FontWeight.Normal)
+
+        EmailStatus(text = email, fontWeight = FontWeight.ExtraBold)
+
+        EmailStatus(
+            text = "Check your email and click on the \n" +
+                   " confirmation link to continue.",
+            fontWeight = FontWeight.Normal)
         
-        Text(text = "We sent confirmation email to:",
-            color = MaterialTheme.colors.onBackground,
-            fontSize = 16.sp,
-            fontWeight = FontWeight.Normal,
-            textAlign = TextAlign.Center)
-
-        Text(text = email,
-            color = MaterialTheme.colors.onBackground,
-            fontSize = 16.sp,
-            fontWeight = FontWeight.ExtraBold,
-            textAlign = TextAlign.Center)
-
-        Text(text = "Check your email and click on the \n" +
-                " confirmation link to continue.",
-            color = MaterialTheme.colors.onBackground,
-            fontSize = 16.sp,
-            fontWeight = FontWeight.Normal,
-            textAlign = TextAlign.Center)
-
-
-
-
-
-
-
     }
+}
+
+
+@Composable
+private fun EmailStatus(modifier: Modifier = Modifier, text: String, fontWeight: FontWeight) {
+    Text(
+        modifier = modifier,
+        text = text,
+        color = MaterialTheme.colors.onBackground,
+        fontSize = 16.sp,
+        fontWeight = fontWeight,
+        textAlign = TextAlign.Center)
+
 }
 
 @Preview
