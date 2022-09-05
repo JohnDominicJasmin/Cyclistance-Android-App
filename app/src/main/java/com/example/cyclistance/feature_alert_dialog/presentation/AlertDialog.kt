1cee7c909f317d327c6e198259e43abad1a398c6
John_Dominic <johndominicjasmin@gmail.com>
2022-09-05 22:14:49 +0800

    Change remember to rememberSaveable.

--- app/src/main/java/com/example/cyclistance/feature_alert_dialog/presentation/AlertDialog.kt
+++ app/src/main/java/com/example/cyclistance/feature_alert_dialog/presentation/AlertDialog.kt
@@ -14,7 +14,7 @@ import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
+import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.shadow
@@ -41,7 +41,7 @@ fun AlertDialog(
     alertDialog: AlertDialogModel,
     onDismissRequest: () -> Unit) {
 
-    val openDialog = remember { mutableStateOf(true) }
+    val openDialog = rememberSaveable { mutableStateOf(true) }
 
 
     if (openDialog.value) {
