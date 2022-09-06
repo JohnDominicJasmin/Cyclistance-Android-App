36402ccdd88b66b560fd232be4a1d0139367c92b
John_Dominic <johndominicjasmin@gmail.com>
2022-09-06 22:55:45 +0800

    Revert "Remove SignIn TextFieldValue State and ShowAlertDialog Event."
    This reverts commit bc73ac46fefe124387876748a5473b9cdea2913c.

--- app/src/main/java/com/example/cyclistance/feature_authentication/presentation/authentication_sign_in/SignInUiEvent.kt
+++ app/src/main/java/com/example/cyclistance/feature_authentication/presentation/authentication_sign_in/SignInUiEvent.kt
@@ -1,10 +1,14 @@
 package com.example.cyclistance.feature_authentication.presentation.authentication_sign_in
 
-
+import androidx.annotation.RawRes
 
 sealed class SignInUiEvent {
     object RefreshEmail: SignInUiEvent()
     object ShowNoInternetScreen : SignInUiEvent()
+    data class ShowAlertDialog(
+        val title: String = "",
+        val description: String = "",
+        @RawRes val imageResId: Int = -1) : SignInUiEvent()
     object ShowMappingScreen: SignInUiEvent()
     data class ShowToastMessage(val message: String) : SignInUiEvent()
 
