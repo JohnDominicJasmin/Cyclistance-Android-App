13a700d09cf3974149431a8f17b51237d0292140
John_Dominic <johndominicjasmin@gmail.com>
2022-09-07 00:56:20 +0800

    Remove ShowAlertDialog Event and TextFieldValue State

--- app/src/main/java/com/example/cyclistance/feature_authentication/presentation/authentication_sign_in/SignInEvent.kt
+++ app/src/main/java/com/example/cyclistance/feature_authentication/presentation/authentication_sign_in/SignInEvent.kt
@@ -1,15 +1,14 @@
 package com.example.cyclistance.feature_authentication.presentation.authentication_sign_in
 
 import android.content.Context
-import androidx.compose.ui.text.input.TextFieldValue
 import com.google.firebase.auth.AuthCredential
 
 sealed class SignInEvent{
     data class SignInFacebook(val context: Context): SignInEvent()
     data class SignInGoogle(var authCredential: AuthCredential): SignInEvent()
     object SignInDefault: SignInEvent()
-    data class EnteredEmail(val email: TextFieldValue) : SignInEvent()
-    data class EnteredPassword(val password: TextFieldValue) : SignInEvent()
-    object ClearEmail: SignInEvent()
+    data class EnterEmail(val email: String) : SignInEvent()
+    data class EnterPassword(val password: String) : SignInEvent()
     object TogglePasswordVisibility: SignInEvent()
+    object DismissAlertDialog : SignInEvent()
 }
