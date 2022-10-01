b341afd613994d3da351e0c70bbd1cef49de7441
John_Dominic <johndominicjasmin@gmail.com>
2022-10-01 18:02:15 +0800

    Remove SharedLocationManager and GetUserLocationUseCase.

--- app/src/main/java/com/example/cyclistance/core/utils/SharedLocationModel.kt
+++
@@ -1,9 +0,0 @@
-package com.example.cyclistance.core.utils
-
-import android.location.Address
-import com.google.android.gms.maps.model.LatLng
-
-data class SharedLocationModel(
-    val latLng: LatLng,
-    val addresses: List<Address>
-)
