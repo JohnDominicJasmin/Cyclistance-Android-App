f7867d23efd0fe82567bfe8ba9619112076e7707
John_Dominic <johndominicjasmin@gmail.com>
2022-10-06 22:06:28 +0800

    Add annotation WorkerThread and try catch.

--- app/src/main/java/com/example/cyclistance/feature_mapping_screen/presentation/mapping_main_screen/utils/MappingUtils.kt
+++ app/src/main/java/com/example/cyclistance/feature_mapping_screen/presentation/mapping_main_screen/utils/MappingUtils.kt
@@ -4,17 +4,27 @@ import android.location.Address
 import android.location.Geocoder
 import android.location.Location
 import android.os.Build
+import androidx.annotation.WorkerThread
+import timber.log.Timber
+import java.io.IOException
 
+@WorkerThread
 fun Geocoder.getAddress(location: Location, onCallbackAddress: (List<Address>) -> Unit) {
+    
+    try {
 
-    if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU) {
-        getFromLocation(
-            location.latitude, location.longitude, 1,
-        ) { addresses ->
-            onCallbackAddress(addresses)
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU) {
+            getFromLocation(
+                location.latitude, location.longitude, 1,
+            ) { addresses ->
+                onCallbackAddress(addresses)
+            }
+        } else {
+            onCallbackAddress(
+                getFromLocation(location.latitude, location.longitude, 1) ?: emptyList())
         }
-    } else {
-        onCallbackAddress(
-            getFromLocation(location.latitude, location.longitude, 1) ?: emptyList())
+
+    } catch (e: IOException) {
+        Timber.e("GET ADDRESS: ${e.message}")
     }
 }
