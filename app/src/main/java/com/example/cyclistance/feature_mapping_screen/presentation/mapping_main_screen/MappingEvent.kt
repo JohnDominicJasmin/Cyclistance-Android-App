949cd316f288f9ff16d284dd6c6af72a9f0e1607
John_Dominic <johndominicjasmin@gmail.com>
2022-10-06 22:06:32 +0800

    Add mapping events.

--- app/src/main/java/com/example/cyclistance/feature_mapping_screen/presentation/mapping_main_screen/MappingEvent.kt
+++ app/src/main/java/com/example/cyclistance/feature_mapping_screen/presentation/mapping_main_screen/MappingEvent.kt
@@ -6,6 +6,9 @@ import android.location.Location
 sealed class MappingEvent {
     object UploadProfile : MappingEvent()
     object SignOut: MappingEvent()
+    object StartPinging: MappingEvent()
+    object StopPinging: MappingEvent()
+    object GetUsersAsynchronously: MappingEvent()
     data class ChangeBottomSheet(val bottomSheetType: String): MappingEvent()
     object LocationPermissionGranted: MappingEvent()
     data class OnLocationChange(val userLocation: Location): MappingEvent()
