2b5d508a13f095f5f41c4113d3c35b04c4caa7f2
John_Dominic <johndominicjasmin@gmail.com>
2022-10-08 19:52:30 +0800

    Change scope and remove SharedLocationManager dependency.

--- app/src/main/java/com/example/cyclistance/di/AuthenticationModule.kt
+++ app/src/main/java/com/example/cyclistance/di/AuthenticationModule.kt
@@ -4,7 +4,7 @@ import android.content.Context
 import com.example.cyclistance.BuildConfig
 import com.example.cyclistance.feature_authentication.data.repository.AuthRepositoryImpl
 import com.example.cyclistance.feature_authentication.domain.repository.AuthRepository
-import com.example.cyclistance.feature_authentication.domain.use_case.*
+import com.example.cyclistance.feature_authentication.domain.use_case.AuthenticationUseCase
 import com.example.cyclistance.feature_authentication.domain.use_case.create_account.*
 import com.example.cyclistance.feature_authentication.domain.use_case.read_account.*
 import com.example.cyclistance.feature_authentication.domain.use_case.sign_out_account.SignOutUseCase
@@ -14,18 +14,18 @@ import com.google.firebase.auth.FirebaseAuth
 import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
-import dagger.hilt.android.components.ViewModelComponent
 import dagger.hilt.android.qualifiers.ApplicationContext
-import dagger.hilt.android.scopes.ViewModelScoped
+import dagger.hilt.components.SingletonComponent
+import javax.inject.Singleton
 
 
 @Module
-@InstallIn(ViewModelComponent::class)
+@InstallIn(SingletonComponent::class)
  object AuthenticationModule {
 
 
     @Provides
-    @ViewModelScoped
+    @Singleton
     fun provideFirebaseAuth():FirebaseAuth{
         return FirebaseAuth.getInstance().apply{
             if(BuildConfig.DEBUG){
@@ -35,7 +35,7 @@ import dagger.hilt.android.scopes.ViewModelScoped
     }
 
     @Provides
-    @ViewModelScoped
+    @Singleton
      fun provideAuthRepository(@ApplicationContext context:Context, firebaseAuth: FirebaseAuth): AuthRepository<AuthCredential>{
          return AuthRepositoryImpl(context = context, auth = firebaseAuth, )
      }
@@ -43,7 +43,7 @@ import dagger.hilt.android.scopes.ViewModelScoped
 
 
     @Provides
-    @ViewModelScoped
+    @Singleton
     fun provideAuthenticationUseCase(repository: AuthRepository<AuthCredential>, @ApplicationContext context: Context):AuthenticationUseCase =
         AuthenticationUseCase(
             reloadEmailUseCase = ReloadEmailUseCase(repository = repository),
--- app/src/main/java/com/example/cyclistance/di/MappingModule.kt
+++ app/src/main/java/com/example/cyclistance/di/MappingModule.kt
@@ -1,12 +1,10 @@
 package com.example.cyclistance.di
 
 import android.content.Context
-import android.location.Geocoder
-import com.example.cyclistance.BaseApplication
 import com.example.cyclistance.BuildConfig
 import com.example.cyclistance.R
 import com.example.cyclistance.core.utils.constants.MappingConstants
-import com.example.cyclistance.core.utils.location.SharedLocationManager
+import com.example.cyclistance.core.utils.location.LocationClient
 import com.example.cyclistance.feature_main_screen.domain.use_case.location.GetUserLocationUseCase
 import com.example.cyclistance.feature_mapping_screen.data.CyclistanceApi
 import com.example.cyclistance.feature_mapping_screen.data.repository.MappingRepositoryImpl
@@ -19,8 +17,6 @@ import com.example.cyclistance.feature_mapping_screen.domain.use_case.bike_type.
 import com.example.cyclistance.feature_mapping_screen.domain.use_case.bike_type.UpdateBikeTypeUseCase
 import com.example.cyclistance.feature_mapping_screen.presentation.mapping_main_screen.utils.MapUiComponents
 import com.google.gson.GsonBuilder
-import com.mapbox.android.core.location.LocationEngineProvider
-import com.mapbox.android.core.location.LocationEngineRequest
 import com.mapbox.maps.plugin.annotation.generated.PointAnnotationOptions
 import dagger.Module
 import dagger.Provides
@@ -29,7 +25,6 @@ import dagger.hilt.android.qualifiers.ApplicationContext
 import dagger.hilt.components.SingletonComponent
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
-import java.util.*
 import javax.inject.Singleton
 
 @Module
@@ -55,43 +50,23 @@ object MappingModule {
     @Singleton
     fun provideCyclistanceRepository(
         @ApplicationContext context: Context,
-        sharedLocationManager: SharedLocationManager,
         api: CyclistanceApi): MappingRepository {
-        return MappingRepositoryImpl(sharedLocationManager = sharedLocationManager, api = api, context = context)
+        return MappingRepositoryImpl(
+            api = api,
+            context = context)
     }
 
     @Provides
     @Singleton
     fun provideLocationEngine(@ApplicationContext context: Context): MapUiComponents {
 
-        val locationEngine = LocationEngineProvider.getBestLocationEngine(context)
-
-        val locationEngineRequest = LocationEngineRequest.Builder(MappingConstants.LOCATION_UPDATES_INTERVAL)
-            .setPriority(LocationEngineRequest.PRIORITY_BALANCED_POWER_ACCURACY)
-            .setDisplacement(2f)
-            .setFastestInterval(MappingConstants.FASTEST_LOCATION_UPDATES_INTERVAL)
-            .build()
-
-
-
-
-
-        //todo remove later
         return MapUiComponents(
-            locationEngine = locationEngine,
-            locationEngineRequest = locationEngineRequest,
-            transitionOptions = {
-                duration = 1000
-            },
             pointAnnotationOptions = PointAnnotationOptions()
                 .withIconSize(1.2)
-
-
-
-
         )
 
     }
+
     @Provides
     @Singleton
     fun provideMappingUseCase(repository: MappingRepository): MappingUseCase {
@@ -109,18 +84,8 @@ object MappingModule {
         )
     }
 
-    @Provides
-    @Singleton
-    fun provideSharedLocationManager(
-        @ApplicationContext context: Context
-    ): SharedLocationManager =
-        SharedLocationManager(context, (context.applicationContext as BaseApplication).applicationScope)
 
 
 
-    @Provides
-    @Singleton
-    fun providesGeocoder(@ApplicationContext context: Context): Geocoder =
-        Geocoder(context, Locale.ENGLISH)
 
 }
