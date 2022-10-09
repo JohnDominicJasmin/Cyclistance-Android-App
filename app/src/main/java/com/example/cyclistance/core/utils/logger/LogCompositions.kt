12f6e787304642d403b3d7d5d5f8a63087f2bd9c
John_Dominic <johndominicjasmin@gmail.com>
2022-10-09 22:06:00 +0800

    Add composition logger.

---
+++ app/src/main/java/com/example/cyclistance/core/utils/logger/LogCompositions.kt
@@ -0,0 +1,18 @@
+package com.example.cyclistance.core.utils.logger
+
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.SideEffect
+import androidx.compose.runtime.remember
+import com.example.cyclistance.BuildConfig
+import timber.log.Timber
+
+class Ref(var value: Int)
+
+@Composable
+inline fun LogCompositions(tag: String, msg: String) {
+    if (BuildConfig.DEBUG) {
+        val ref = remember { Ref(0) }
+        SideEffect { ref.value++ }
+        Timber.d(tag, "Compositions: $msg ${ref.value}")
+    }
+}
