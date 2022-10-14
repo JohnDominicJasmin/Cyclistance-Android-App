be06a88f05b3b56c4d7efa2ba83326123743225a
John_Dominic <johndominicjasmin@gmail.com>
2022-10-15 00:11:03 +0800

    Rename Event.

--- app/src/main/java/com/example/cyclistance/feature_mapping_screen/presentation/mapping_main_screen/MappingEvent.kt
+++ app/src/main/java/com/example/cyclistance/feature_mapping_screen/presentation/mapping_main_screen/MappingEvent.kt
@@ -1,14 +1,12 @@
 package com.example.cyclistance.feature_mapping_screen.presentation.mapping_main_screen
 
-import com.example.cyclistance.feature_authentication.presentation.authentication_email.EmailAuthEvent
-
 
 sealed class MappingEvent {
     object UploadProfile : MappingEvent()
     object SignOut: MappingEvent()
     object StartPinging: MappingEvent()
     object StopPinging: MappingEvent()
-    object GetUsersAsynchronously: MappingEvent()
+    object GetUsers: MappingEvent()
     data class ChangeBottomSheet(val bottomSheetType: String): MappingEvent()
     object SubscribeToLocationUpdates: MappingEvent()
     object UnsubscribeToLocationUpdates: MappingEvent()
