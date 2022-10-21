70cfd17fa2837a58f1836b77c9b8c1a86779bd48
John_Dominic <johndominicjasmin@gmail.com>
2022-10-21 14:13:26 +0800

    Add Validation for Phone Number.

--- app/src/main/java/com/example/cyclistance/feature_authentication/domain/use_case/create_account/UpdatePhoneNumberUseCase.kt
+++ app/src/main/java/com/example/cyclistance/feature_authentication/domain/use_case/create_account/UpdatePhoneNumberUseCase.kt
@@ -1,14 +1,37 @@
 package com.example.cyclistance.feature_authentication.domain.use_case.create_account
 
+import com.example.cyclistance.core.utils.InputValidate.containsSpecialCharacters
+import com.example.cyclistance.core.utils.InputValidate.isDigit
+import com.example.cyclistance.core.utils.InputValidate.isPhoneNumberLongEnough
 import com.example.cyclistance.feature_authentication.domain.repository.AuthRepository
 import com.example.cyclistance.feature_mapping_screen.domain.exceptions.MappingExceptions
 import com.google.firebase.auth.AuthCredential
 
 class UpdatePhoneNumberUseCase(private val repository: AuthRepository<AuthCredential>) {
     suspend operator fun invoke(phoneNumber:String){
-        if(phoneNumber.isEmpty()){
+
+        val userPhoneNumber = phoneNumber.trim()
+
+        if(userPhoneNumber.isEmpty()){
             throw MappingExceptions.PhoneNumberException()
         }
+
+        if(!userPhoneNumber.isDigit()){
+            throw MappingExceptions.PhoneNumberException("Phone number must contain only numbers.")
+        }
+
+        if(userPhoneNumber.containsSpecialCharacters()){
+            throw MappingExceptions.PhoneNumberException("Phone number must not contain special characters.")
+        }
+
+        if(!userPhoneNumber.isPhoneNumberLongEnough()){
+            throw MappingExceptions.PhoneNumberException("Phone number is invalid")
+        }
+
+        if(!userPhoneNumber.startsWith(prefix = "9")){
+            throw MappingExceptions.PhoneNumberException("Phone number must start with 9")
+        }
+
         repository.updatePhoneNumber(phoneNumber)
     }
 }
