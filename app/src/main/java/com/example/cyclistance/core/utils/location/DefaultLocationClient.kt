a1c4386eda3aa7678fd8e161f26bcb245d354ff7
John_Dominic <johndominicjasmin@gmail.com>
2022-10-27 19:43:32 +0800

    Change FusedLocationProviderClient(context)

--- app/src/main/java/com/example/cyclistance/core/utils/location/DefaultLocationClient.kt
+++ app/src/main/java/com/example/cyclistance/core/utils/location/DefaultLocationClient.kt
@@ -3,10 +3,10 @@ import android.annotation.SuppressLint
 import android.content.Context
 import android.location.Location
 import android.os.Looper
-import com.google.android.gms.location.FusedLocationProviderClient
 import com.google.android.gms.location.LocationCallback
 import com.google.android.gms.location.LocationRequest
 import com.google.android.gms.location.LocationResult
+import com.google.android.gms.location.LocationServices
 import kotlinx.coroutines.channels.awaitClose
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.callbackFlow
@@ -15,8 +15,8 @@ class DefaultLocationClient(
     private val locationRequest: LocationRequest,
     val context: Context):LocationClient {
 
-    private val fusedLocationProviderClient: FusedLocationProviderClient by lazy {
-        FusedLocationProviderClient(context)
+    private val fusedLocationProviderClient by lazy {
+        LocationServices.getFusedLocationProviderClient(context)
     }
 
 
