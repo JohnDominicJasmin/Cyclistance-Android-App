a07b9935baf617b7ac2b1067fac05d9cd0dd8631
John_Dominic <johndominicjasmin@gmail.com>
2022-10-27 19:48:24 +0800

    Set data class parameters to nullable.

--- app/src/main/java/com/example/cyclistance/feature_mapping_screen/domain/model/CardModel.kt
+++ app/src/main/java/com/example/cyclistance/feature_mapping_screen/domain/model/CardModel.kt
@@ -1,11 +1,11 @@
 package com.example.cyclistance.feature_mapping_screen.domain.model
 
 data class CardModel(
-    val id: String = "",
-    val name: String = "",
-    val profileImage:String = "",
-    val estimatedTimeTravel: String = "",
-    val distance:String = "",
-    val address:String = "",
+    val id: String? = null,
+    val name: String? = null,
+    val profileImage:String? = null,
+    val estimatedTimeTravel: String? = null,
+    val distance:String? = null,
+    val address:String? = null,
 
     )
