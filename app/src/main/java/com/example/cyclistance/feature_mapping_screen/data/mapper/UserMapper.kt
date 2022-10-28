9ac8d202631f63f41670e1dba9eb94034b0b289f
John_Dominic <johndominicjasmin@gmail.com>
2022-10-29 00:34:08 +0800

    Add Mapping toUserDto.

--- app/src/main/java/com/example/cyclistance/feature_mapping_screen/data/mapper/UserMapper.kt
+++ app/src/main/java/com/example/cyclistance/feature_mapping_screen/data/mapper/UserMapper.kt
@@ -1,33 +1,48 @@
 package com.example.cyclistance.feature_mapping_screen.data.mapper
 
-import com.example.cyclistance.feature_mapping_screen.data.remote.dto.*
+import com.example.cyclistance.feature_mapping_screen.data.remote.dto.user_dto.UserDto
 import com.example.cyclistance.feature_mapping_screen.domain.model.*
 
 object UserMapper {
 
-fun UserDto.toUser():User{
-    return User(
-        address = this.address,
-        contactNumber = this.contactNumber,
-        id = this.id,
-        location = this.location,
-        name = this.name,
-        profilePictureUrl = this.profilePictureUrl,
-        userNeededHelp = this.userNeededHelp,
-        userAssistance = this.userAssistance,
-        rescueRequest = this.rescueRequest,
+    fun UserDto.toUser(): User {
+        return User(
+            address = this.address,
+            contactNumber = this.contactNumber,
+            id = this.id,
+            location = this.location,
+            name = this.name,
+            profilePictureUrl = this.profilePictureUrl,
+            userAssistance = this.userAssistance,
+            rescueRequest = this.rescueRequest,
+            transaction = this.transaction
+            )
+    }
 
-    )
-}
-}
 
-fun User.toCardModel(): CardModel{
-    return CardModel(
-        id = this.id,
-        name = this.name,
-        profileImageUrl = this.profilePictureUrl,
-        distance = "0.5km",
-        estimatedTimeTravel = "5 mins",
-        address = this.address
-    )
+    fun User.toUserDto():UserDto{
+        return UserDto(
+            address = this.address,
+            contactNumber = this.contactNumber,
+            id = this.id,
+            location = this.location,
+            name = this.name,
+            profilePictureUrl = this.profilePictureUrl,
+            userAssistance = this.userAssistance,
+            rescueRequest = this.rescueRequest,
+            transaction = this.transaction
+        )
+    }
+
+
+    fun User.toCardModel(): CardModel {
+        return CardModel(
+            id = this.id,
+            name = this.name,
+            profileImageUrl = this.profilePictureUrl,
+            distance = "0.5km", //todo: compute later
+            estimatedTimeTravel = "5 mins",
+            address = this.address
+        )
+    }
 }
