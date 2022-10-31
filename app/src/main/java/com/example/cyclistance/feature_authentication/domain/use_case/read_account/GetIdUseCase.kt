262bf5fc4996d34894c2b45234ac30b250da6a03
John_Dominic <johndominicjasmin@gmail.com>
2022-10-31 23:29:42 +0800

    Throw exception if id is null or empty.

--- app/src/main/java/com/example/cyclistance/feature_authentication/domain/use_case/read_account/GetIdUseCase.kt
+++ app/src/main/java/com/example/cyclistance/feature_authentication/domain/use_case/read_account/GetIdUseCase.kt
@@ -1,10 +1,12 @@
 package com.example.cyclistance.feature_authentication.domain.use_case.read_account
 
 import com.example.cyclistance.feature_authentication.domain.repository.AuthRepository
+import com.example.cyclistance.feature_mapping_screen.domain.exceptions.MappingExceptions
 import com.google.firebase.auth.AuthCredential
 
 class GetIdUseCase(private val repository: AuthRepository<AuthCredential>) {
-    operator fun invoke():String? {
-        return repository.getId()
+    operator fun invoke():String {
+        return repository.getId().takeIf { !it.isNullOrEmpty() }
+               ?: throw MappingExceptions.UserException()
     }
 }
