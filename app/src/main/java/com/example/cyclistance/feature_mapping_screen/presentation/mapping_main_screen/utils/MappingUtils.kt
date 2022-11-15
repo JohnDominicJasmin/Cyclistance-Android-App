2e133149e9260a364ab541cf8d9180a07f5673e1
John_Dominic <johndominicjasmin@gmail.com>
2022-11-16 00:08:16 +0800

    Add extension functions.

--- app/src/main/java/com/example/cyclistance/feature_mapping_screen/presentation/mapping_main_screen/utils/MappingUtils.kt
+++ app/src/main/java/com/example/cyclistance/feature_mapping_screen/presentation/mapping_main_screen/utils/MappingUtils.kt
@@ -1,9 +1,35 @@
 package com.example.cyclistance.feature_mapping_screen.presentation.mapping_main_screen.utils
 
+import android.content.Context
+import android.content.Intent
 import android.location.Address
 import android.location.Geocoder
 import android.os.Build
 import androidx.annotation.WorkerThread
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.remember
+import androidx.core.content.ContextCompat
+import com.example.cyclistance.core.utils.constants.MappingConstants
+import com.example.cyclistance.core.utils.service.LocationService
+import com.mapbox.api.directions.v5.models.Bearing
+import com.mapbox.api.directions.v5.models.DirectionsRoute
+import com.mapbox.api.directions.v5.models.RouteOptions
+import com.mapbox.geojson.Point
+import com.mapbox.maps.MapView
+import com.mapbox.maps.plugin.PuckBearingSource
+import com.mapbox.maps.plugin.attribution.attribution
+import com.mapbox.maps.plugin.locationcomponent.location2
+import com.mapbox.maps.plugin.logo.logo
+import com.mapbox.maps.plugin.scalebar.scalebar
+import com.mapbox.navigation.base.extensions.applyDefaultNavigationOptions
+import com.mapbox.navigation.base.extensions.applyLanguageAndVoiceUnitOptions
+import com.mapbox.navigation.base.options.NavigationOptions
+import com.mapbox.navigation.base.route.RouterCallback
+import com.mapbox.navigation.base.route.RouterFailure
+import com.mapbox.navigation.base.route.RouterOrigin
+import com.mapbox.navigation.core.MapboxNavigation
+import com.mapbox.navigation.core.MapboxNavigationProvider
+import com.mapbox.navigation.ui.maps.location.NavigationLocationProvider
 import timber.log.Timber
 import java.io.IOException
 
@@ -29,6 +55,71 @@ inline fun Geocoder.getAddress(
     } catch (e: IOException) {
         Timber.e("GET ADDRESS: ${e.message}")
     }
+}
+
+fun Context.startLocationServiceIntentAction(intentAction:String = MappingConstants.ACTION_START){
+    Intent(this, LocationService::class.java).apply {
+        action = intentAction
+        startService(this)
+    }
+}
+
+fun Address.getFullAddress(): String {
+    val subThoroughfare = if(subThoroughfare != "null" && subThoroughfare != null) "$subThoroughfare " else ""
+    val thoroughfare = if (thoroughfare != "null" && thoroughfare != null) "$thoroughfare., " else ""
+    val locality = if (locality != "null" && locality != null) "$locality, " else ""
+    val subAdminArea = if(subAdminArea != "null" && subAdminArea != null) subAdminArea else ""
+
+
+    return "$subThoroughfare$thoroughfare$locality$subAdminArea"
+}
+
+inline fun MapboxNavigation.findRoute(
+    parentContext: Context,
+    originPoint: Point,
+    destinationPoint: Point,
+    crossinline onSuccess: (List<DirectionsRoute>) -> Unit){
+
+    requestRoutes(
+        RouteOptions.builder()
+
+            .applyDefaultNavigationOptions()
+            .applyLanguageAndVoiceUnitOptions(parentContext)
+            .coordinatesList(listOf(originPoint, destinationPoint))
+            .bearingsList(
+                listOf(
+                    Bearing.builder()
+                        .angle(0.0)
+                        .degrees(45.0)
+                        .build(),
+                    null
+                )
+            )
+            .layersList(listOf(getZLevel(), null))
+            .build(),
+        object : RouterCallback {
+            override fun onRoutesReady(
+                routes: List<DirectionsRoute>,
+                routerOrigin: RouterOrigin
+            ) {
+                onSuccess(routes)
+            }
+
+            override fun onFailure(
+                reasons: List<RouterFailure>,
+                routeOptions: RouteOptions
+            ) {
+                Timber.e("Route request failed")
+            }
+
+            override fun onCanceled(
+                routeOptions: RouteOptions,
+                routerOrigin: RouterOrigin) {
+
+                Timber.e("Route request canceled")
+            }
+        }
+    )
 }
 
 fun Double.distanceFormat(): String {
@@ -57,3 +148,34 @@ fun getETA(distanceMeters: Double, averageSpeedKm: Double): String {
     return "$hourFormat$minsFormat"
 }
 
+fun MapView.setDefaultSettings(parentContext: Context, navigationLocationProvider: NavigationLocationProvider){
+    scalebar.enabled = false
+    logo.enabled = false
+    attribution.enabled = false
+
+    location2.apply {
+
+        showAccuracyRing = true
+        pulsingColor = ContextCompat.getColor(parentContext, com.example.cyclistance.R.color.ThemeColor)
+        puckBearingEnabled = false
+        pulsingMaxRadius = 120.0f
+        puckBearingSource = PuckBearingSource.HEADING
+        setLocationProvider(navigationLocationProvider)
+        enabled = true
+    }
+}
+
+@Composable
+fun rememberMapboxNavigation(parentContext: Context):MapboxNavigation{
+    return remember{
+        if (MapboxNavigationProvider.isCreated()) {
+            MapboxNavigationProvider.retrieve()
+        } else {
+            MapboxNavigationProvider.create(
+                NavigationOptions.Builder(parentContext.applicationContext)
+                    .accessToken(parentContext.getString(com.example.cyclistance.R.string.MapsDownloadToken))
+                    .build()
+            )
+        }
+    }
+}
