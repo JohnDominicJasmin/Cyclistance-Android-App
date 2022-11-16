044e51ddbb4da53504b4be0bf3dec4c58703fed9
John_Dominic <johndominicjasmin@gmail.com>
2022-11-17 01:34:02 +0800

    Remove maneuverView visibility.

--- app/src/main/java/com/example/cyclistance/feature_mapping_screen/presentation/mapping_main_screen/components/MappingMapsScreen.kt
+++ app/src/main/java/com/example/cyclistance/feature_mapping_screen/presentation/mapping_main_screen/components/MappingMapsScreen.kt
@@ -217,7 +217,6 @@ fun MappingMapsScreen(
                     ).show()
                 },
                 {
-                    maneuverView.visibility = View.VISIBLE
                     maneuverView.renderManeuvers(maneuvers)
                 }
             )
