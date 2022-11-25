f800fae9d36742d19b5e141268e3573b4f2e0492
John_Dominic <johndominicjasmin@gmail.com>
2022-11-26 00:23:11 +0800

    Rename function

--- app/src/main/java/com/example/cyclistance/feature_mapping_screen/domain/use_case/websockets/rescue_transactions/BroadcastRescueTransactionUseCase.kt
+++ app/src/main/java/com/example/cyclistance/feature_mapping_screen/domain/use_case/websockets/rescue_transactions/BroadcastRescueTransactionUseCase.kt
@@ -1,9 +1,9 @@
-package com.example.cyclistance.feature_mapping_screen.domain.use_case.websockets
+package com.example.cyclistance.feature_mapping_screen.domain.use_case.websockets.rescue_transactions
 
 import com.example.cyclistance.feature_mapping_screen.domain.repository.MappingRepository
 
 class BroadcastRescueTransactionUseCase(private val repository: MappingRepository) {
     operator fun invoke() {
-        repository.broadCastRescueTransaction()
+        repository.broadcastRescueTransaction()
     }
 }
--- app/src/main/java/com/example/cyclistance/feature_mapping_screen/domain/use_case/websockets/users/BroadcastUserUseCase.kt
+++ app/src/main/java/com/example/cyclistance/feature_mapping_screen/domain/use_case/websockets/users/BroadcastUserUseCase.kt
@@ -1,9 +1,9 @@
-package com.example.cyclistance.feature_mapping_screen.domain.use_case.websockets
+package com.example.cyclistance.feature_mapping_screen.domain.use_case.websockets.users
 
 import com.example.cyclistance.feature_mapping_screen.domain.repository.MappingRepository
 
 class BroadcastUserUseCase(private val repository: MappingRepository) {
     operator fun invoke() {
-        repository.broadCastUser()
+        repository.broadcastUser()
     }
 }
