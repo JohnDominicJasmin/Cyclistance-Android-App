3c5f93fb5f5a26783550c778c8dca2df4d03a907
John_Dominic <johndominicjasmin@gmail.com>
2022-12-02 22:09:53 +0800

    Add parcelable, immutable and stable annotation.

--- app/src/main/java/com/example/cyclistance/feature_mapping_screen/presentation/mapping_cancellation_reason/CancellationReasonState.kt
+++ app/src/main/java/com/example/cyclistance/feature_mapping_screen/presentation/mapping_cancellation_reason/CancellationReasonState.kt
@@ -1,10 +1,17 @@
 package com.example.cyclistance.feature_mapping_screen.presentation.mapping_cancellation_reason
 
+import android.os.Parcelable
+import androidx.compose.runtime.Immutable
+import androidx.compose.runtime.Stable
+import kotlinx.parcelize.Parcelize
+
+@Parcelize
+@Immutable
+@Stable
 data class CancellationReasonState(
     val selectedReason: String = "",
     val hasInternet: Boolean = true,
     val isLoading: Boolean = false,
     val reasonErrorMessage: String = "",
     val message: String = "",
-
-    )
+):Parcelable
