691bf69340ebc46a42a38235789c907a9fcdeb59
John_Dominic <johndominicjasmin@gmail.com>
2022-12-03 19:10:25 +0800

    Remove function that updates user transaction when cancellation rescue transaction.

--- app/src/main/java/com/example/cyclistance/feature_mapping_screen/presentation/mapping_cancellation_reason/CancellationReasonViewModel.kt
+++ app/src/main/java/com/example/cyclistance/feature_mapping_screen/presentation/mapping_cancellation_reason/CancellationReasonViewModel.kt
@@ -9,12 +9,8 @@ import com.example.cyclistance.feature_mapping_screen.data.remote.dto.rescue_tra
 import com.example.cyclistance.feature_mapping_screen.data.remote.dto.rescue_transaction.CancellationReason
 import com.example.cyclistance.feature_mapping_screen.data.remote.dto.rescue_transaction.Route
 import com.example.cyclistance.feature_mapping_screen.data.remote.dto.rescue_transaction.Status
-import com.example.cyclistance.feature_mapping_screen.data.remote.dto.user_dto.RescueRequest
-import com.example.cyclistance.feature_mapping_screen.data.remote.dto.user_dto.Transaction
-import com.example.cyclistance.feature_mapping_screen.data.remote.dto.user_dto.UserAssistance
 import com.example.cyclistance.feature_mapping_screen.domain.exceptions.MappingExceptions
 import com.example.cyclistance.feature_mapping_screen.domain.model.RescueTransactionItem
-import com.example.cyclistance.feature_mapping_screen.domain.model.UserItem
 import com.example.cyclistance.feature_mapping_screen.domain.use_case.MappingUseCase
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.Dispatchers
@@ -84,7 +80,9 @@ class CancellationReasonViewModel @Inject constructor(
 
             }.onSuccess {
                 broadcastRescueTransaction()
-                removeAssignedTransaction()
+                _eventFlow.emit(value = CancellationReasonUiEvent.ShowMappingScreen)
+                delay(500)
+                finishLoading()
             }.onFailure { exception ->
                 finishLoading()
                 exception.handleException()
@@ -93,23 +91,6 @@ class CancellationReasonViewModel @Inject constructor(
             }
         }
     }
-    private fun removeAssignedTransaction(){
-        viewModelScope.launch(Dispatchers.IO) {
-            runCatching {
-                getId().removeAssignedTransaction()
-                _clientId.removeAssignedTransaction()
-            }.onSuccess {
-                broadcastUser()
-                _eventFlow.emit(value = CancellationReasonUiEvent.ShowMappingScreen)
-                delay(500)
-                finishLoading()
-            }.onFailure { exception ->
-                finishLoading()
-                exception.handleException()
-            }
-        }
-
-        }
 
     private fun startLoading() {
         _state.update { it.copy(isLoading = true) }
@@ -120,14 +101,6 @@ class CancellationReasonViewModel @Inject constructor(
     }
 
 
-    private suspend fun String.removeAssignedTransaction(){
-        mappingUseCase.createUserUseCase(
-            user = UserItem(
-                id = this,
-                transaction = Transaction(),
-                userAssistance = UserAssistance(needHelp = false),
-                rescueRequest = RescueRequest()))
-    }
 
     private suspend fun broadcastRescueTransaction() {
         runCatching {
