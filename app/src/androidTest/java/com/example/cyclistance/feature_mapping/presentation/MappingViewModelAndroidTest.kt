1bd28e2e6ebd58e20b6a3e6d99d793850c20a282
John_Dominic <johndominicjasmin@gmail.com>
2023-01-14 23:06:00 +0800

    Move test from unit test to instrumentation test.

--- app/src/androidTest/java/com/example/cyclistance/feature_mapping/presentation/MappingViewModelAndroidTest.kt
+++ app/src/androidTest/java/com/example/cyclistance/feature_mapping/presentation/MappingViewModelAndroidTest.kt
@@ -1,12 +1,9 @@
-package com.example.cyclistance.mapping_main_screen.presentation
+package com.example.cyclistance.feature_mapping.presentation
 
-import android.location.Geocoder
 import androidx.arch.core.executor.testing.InstantTaskExecutorRule
-import androidx.lifecycle.SavedStateHandle
 import androidx.test.platform.app.InstrumentationRegistry
 import com.example.cyclistance.feature_authentication.domain.use_case.AuthenticationUseCase
 import com.example.cyclistance.feature_mapping.domain.use_case.MappingUseCase
-import com.example.cyclistance.feature_mapping.presentation.mapping_main_screen.MappingViewModel
 import dagger.hilt.android.testing.HiltAndroidRule
 import dagger.hilt.android.testing.HiltAndroidTest
 import org.junit.Assert
@@ -18,8 +15,8 @@ import javax.inject.Named
 
 
 @HiltAndroidTest
-class MappingViewModelTest{
-    private lateinit var mappingViewModel: MappingViewModel
+class MappingViewModelAndroidTest{
+
 
     @get:Rule
     val hiltRule = HiltAndroidRule(this)
@@ -38,13 +35,7 @@ class MappingViewModelTest{
     @Before
     fun setUp() {
         val appContext = InstrumentationRegistry.getInstrumentation().targetContext
-
         hiltRule.inject()
-        mappingViewModel = MappingViewModel(
-            authUseCase = authUseCase,
-            mappingUseCase = mappingUseCase,
-            geocoder = Geocoder(appContext),
-            savedStateHandle = SavedStateHandle())
 
     }
 
