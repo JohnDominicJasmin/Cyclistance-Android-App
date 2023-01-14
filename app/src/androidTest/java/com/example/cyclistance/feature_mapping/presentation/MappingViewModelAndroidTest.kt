ff51f4a5f10ab8af02e37d337163b3c3c95b4ad0
John_Dominic <johndominicjasmin@gmail.com>
2023-01-15 00:46:49 +0800

    Comment out unused parameters.

--- app/src/androidTest/java/com/example/cyclistance/feature_mapping/presentation/MappingViewModelAndroidTest.kt
+++ app/src/androidTest/java/com/example/cyclistance/feature_mapping/presentation/MappingViewModelAndroidTest.kt
@@ -1,23 +1,12 @@
 package com.example.cyclistance.feature_mapping.presentation
 
-import androidx.arch.core.executor.testing.InstantTaskExecutorRule
-import androidx.test.platform.app.InstrumentationRegistry
-import com.example.cyclistance.feature_authentication.domain.use_case.AuthenticationUseCase
-import com.example.cyclistance.feature_mapping.domain.use_case.MappingUseCase
-import dagger.hilt.android.testing.HiltAndroidRule
 import dagger.hilt.android.testing.HiltAndroidTest
-import org.junit.Assert
-import org.junit.Before
-import org.junit.Rule
-import org.junit.Test
-import javax.inject.Inject
-import javax.inject.Named
 
 
 @HiltAndroidTest
 class MappingViewModelAndroidTest{
 
-
+/*
     @get:Rule
     val hiltRule = HiltAndroidRule(this)
 
@@ -43,5 +32,5 @@ class MappingViewModelAndroidTest{
     fun assertTrue(){
         Assert.assertTrue(true)
 
-    }
+    }*/
 }
