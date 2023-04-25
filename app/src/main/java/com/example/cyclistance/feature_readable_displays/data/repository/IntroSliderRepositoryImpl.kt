f160aac77daa24b3b9550bd803eda4ac937e3fa6
John_Dominic <johndominicjasmin@gmail.com>
2023-04-25 20:46:19 +0800

    Format code.

--- app/src/main/java/com/example/cyclistance/feature_readable_displays/data/repository/IntroSliderRepositoryImpl.kt
+++ app/src/main/java/com/example/cyclistance/feature_readable_displays/data/repository/IntroSliderRepositoryImpl.kt
@@ -4,23 +4,20 @@ import android.content.Context
 import com.example.cyclistance.core.utils.constants.ReadableConstants.DATA_STORE_INTRO_SLIDER_KEY
 import com.example.cyclistance.core.utils.extension.editData
 import com.example.cyclistance.core.utils.extension.getData
-import com.example.cyclistance.feature_readable_displays.domain.repository.IntroSliderRepository
 import com.example.cyclistance.feature_mapping.data.repository.dataStore
+import com.example.cyclistance.feature_readable_displays.domain.repository.IntroSliderRepository
 import kotlinx.coroutines.flow.Flow
 
 
 class IntroSliderRepositoryImpl(context: Context) : IntroSliderRepository {
     private var dataStore = context.dataStore
 
-
     override fun userCompletedWalkThrough(): Flow<Boolean> {
         return dataStore.getData(key = DATA_STORE_INTRO_SLIDER_KEY, defaultValue = false)
-
     }
 
     override suspend fun setUserCompletedWalkThrough() {
         dataStore.editData(DATA_STORE_INTRO_SLIDER_KEY, true)
     }
-
 
 }
