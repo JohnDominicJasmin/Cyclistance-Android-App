39d121d13ae193c2ccb5dd73e99f176ccd71ca39
John_Dominic <johndominicjasmin@gmail.com>
2023-05-30 20:02:07 +0800

    Change implementation of function convert Bitmap to Uri

--- app/src/main/java/com/example/cyclistance/core/utils/save_images/ImageUtils.kt
+++ app/src/main/java/com/example/cyclistance/core/utils/save_images/ImageUtils.kt
@@ -3,9 +3,10 @@ package com.example.cyclistance.core.utils.save_images
 import android.content.Context
 import android.graphics.Bitmap
 import android.net.Uri
-import android.provider.MediaStore
 import timber.log.Timber
 import java.io.ByteArrayOutputStream
+import java.io.File
+import java.io.FileOutputStream
 import java.io.OutputStream
 
 object ImageUtils {
@@ -21,11 +22,17 @@ object ImageUtils {
         }
     }
 
-    fun Bitmap.toImageUri(inContext: Context): Uri? {
+    fun Bitmap.toImageUri(): Uri? {
+        val tempFile = File.createTempFile("cyclistance", ".png")
         val bytes = ByteArrayOutputStream()
-        compress(Bitmap.CompressFormat.JPEG, 100, bytes)
-        val path = MediaStore.Images.Media.insertImage(inContext.contentResolver, this, "CyclistanceImage", null)
-        return Uri.parse(path)
+        compress(Bitmap.CompressFormat.PNG, 100, bytes)
+        val bitmapData = bytes.toByteArray()
+
+        val fileOutPut = FileOutputStream(tempFile)
+        fileOutPut.write(bitmapData)
+        fileOutPut.flush()
+        fileOutPut.close()
+        return Uri.fromFile(tempFile)
     }
 
     fun Context.saveImageToGallery(
