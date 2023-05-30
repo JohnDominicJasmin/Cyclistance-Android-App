91cbc1ebfe3ac56b626e386ebf9658ac3a28a94f
John_Dominic <67941562+JohnDominicJasmin@users.noreply.github.com>
2023-05-30 21:23:33 +0800

    Remove SaveImageToGallery feature (#170)

--- app/src/main/java/com/example/cyclistance/core/utils/save_images/ImageSaver.kt
+++
@@ -1,8 +0,0 @@
-package com.example.cyclistance.core.utils.save_images
-
-import android.graphics.Bitmap
-import android.net.Uri
-
-interface ImageSaver {
-    fun saveImage(bitmap: Bitmap): Uri?
-}
