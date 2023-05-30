eca498fe54b0d0e16ddf02ce5f3f38246121e7f0
John_Dominic <johndominicjasmin@gmail.com>
2023-05-30 19:45:51 +0800

    Logs and remove comment

--- app/src/main/java/com/example/cyclistance/feature_authentication/domain/model/AuthModel.kt
+++ app/src/main/java/com/example/cyclistance/feature_authentication/domain/model/AuthModel.kt
@@ -1,7 +1,6 @@
 package com.example.cyclistance.feature_authentication.domain.model
 
 
-// TODO: remove this class later
 data class AuthModel(
     val email:String = "",
     val password:String = "",
