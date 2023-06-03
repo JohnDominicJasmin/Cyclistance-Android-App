464cecf3c9463b6e95f918deec239ccd27aaa7e4
John_Dominic <johndominicjasmin@gmail.com>
2023-06-03 12:17:46 +0800

    Move EditProfileContent to another file

--- app/src/main/java/com/example/cyclistance/feature_settings/presentation/setting_edit_profile/EditProfileScreen.kt
+++ app/src/main/java/com/example/cyclistance/feature_settings/presentation/setting_edit_profile/EditProfileScreen.kt
@@ -12,10 +12,7 @@ import androidx.activity.compose.rememberLauncherForActivityResult
 import androidx.activity.result.contract.ActivityResultContracts
 import androidx.activity.result.launch
 import androidx.compose.foundation.layout.PaddingValues
-import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.size
-import androidx.compose.foundation.text.ClickableText
 import androidx.compose.foundation.text.KeyboardActions
 import androidx.compose.material.*
 import androidx.compose.runtime.*
@@ -24,31 +21,16 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.asImageBitmap
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.platform.LocalSoftwareKeyboardController
-import androidx.compose.ui.text.SpanStyle
-import androidx.compose.ui.text.buildAnnotatedString
-import androidx.compose.ui.text.font.FontWeight
-import androidx.compose.ui.text.withStyle
-import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.unit.dp
-import androidx.constraintlayout.compose.ConstraintLayout
-import androidx.constraintlayout.compose.Dimension
 import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.lifecycle.compose.collectAsStateWithLifecycle
 import androidx.navigation.NavController
 import com.example.cyclistance.core.utils.permissions.requestPermission
 import com.example.cyclistance.core.utils.save_images.ImageUtils.toImageUri
-import com.example.cyclistance.feature_dialogs.presentation.no_internet_dialog.NoInternetDialog
-import com.example.cyclistance.feature_mapping.presentation.common.MappingButtonNavigation
-import com.example.cyclistance.feature_settings.presentation.setting_edit_profile.components.ProfilePictureArea
-import com.example.cyclistance.feature_settings.presentation.setting_edit_profile.components.SelectImageBottomSheet
-import com.example.cyclistance.feature_settings.presentation.setting_edit_profile.components.TextFieldInputArea
+import com.example.cyclistance.feature_settings.presentation.setting_edit_profile.components.EditProfileScreenContent
 import com.example.cyclistance.feature_settings.presentation.setting_edit_profile.event.EditProfileEvent
 import com.example.cyclistance.feature_settings.presentation.setting_edit_profile.event.EditProfileUiEvent
 import com.example.cyclistance.feature_settings.presentation.setting_edit_profile.event.EditProfileVmEvent
-import com.example.cyclistance.feature_settings.presentation.setting_edit_profile.state.EditProfileState
 import com.example.cyclistance.feature_settings.presentation.setting_edit_profile.state.EditProfileUiState
-import com.example.cyclistance.theme.Blue600
-import com.example.cyclistance.theme.CyclistanceTheme
 import com.google.accompanist.permissions.*
 import kotlinx.coroutines.launch
 
@@ -279,172 +261,4 @@ fun EditProfileScreen(
     )
 
 }
-
-@OptIn(ExperimentalMaterialApi::class)
-@Preview(device = "id:Galaxy Nexus")
-@Composable
-fun EditProfilePreview() {
-
-    CyclistanceTheme(true) {
-        EditProfileScreenContent(
-            modifier = Modifier,
-            photoUrl = "",
-            state = EditProfileState(isLoading = false),
-            uiState = EditProfileUiState(),
-            bottomSheetScaffoldState = rememberModalBottomSheetState(ModalBottomSheetValue.Hidden),
-            event = {}
-
-        )
-    }
-}
-
-
-@OptIn(ExperimentalMaterialApi::class)
-@Composable
-fun EditProfileScreenContent(
-    modifier: Modifier,
-    photoUrl: Any,
-    state: EditProfileState = EditProfileState(),
-    keyboardActions: KeyboardActions = KeyboardActions { },
-    uiState: EditProfileUiState,
-    bottomSheetScaffoldState: ModalBottomSheetState,
-    event: (EditProfileUiEvent) -> Unit = {}
-) {
-
-
-
-
-    val isUserInformationChanges by remember(
-        uiState.name,
-        uiState.phoneNumber,
-        uiState.selectedImageUri) {
-        derivedStateOf {
-            uiState.name != state.nameSnapshot ||
-            uiState.phoneNumber != state.phoneNumberSnapshot ||
-            uiState.selectedImageUri.isNotEmpty()
-        }
-    }
-
-
-    Surface(modifier = modifier.fillMaxSize(), color = MaterialTheme.colors.background) {
-
-        SelectImageBottomSheet(
-            onClickGalleryButton = {
-                event(EditProfileUiEvent.ToggleBottomSheet)
-                event(EditProfileUiEvent.SelectImageFromGallery)
-            },
-            onClickCameraButton = {
-                event(EditProfileUiEvent.ToggleBottomSheet)
-                event(EditProfileUiEvent.OpenCamera)
-            },
-            bottomSheetScaffoldState = bottomSheetScaffoldState,
-            editProfileState = state) {
-
-            ConstraintLayout(modifier = Modifier.fillMaxSize()) {
-
-                val (profilePictureArea, textFieldInputArea, buttonNavigationArea, changePhotoText, progressBar, noInternetDialog) = createRefs()
-
-                ProfilePictureArea(
-                    photoUrl = photoUrl,
-                    modifier = Modifier
-                        .size(125.dp)
-                        .constrainAs(profilePictureArea) {
-
-                            top.linkTo(parent.top, margin = 30.dp)
-                            start.linkTo(parent.start)
-                            end.linkTo(parent.end)
-
-                        },
-                    onClick = {
-                        event(EditProfileUiEvent.ToggleBottomSheet)
-                    })
-
-
-
-
-                ClickableText(text = buildAnnotatedString {
-                    withStyle(
-                        style = SpanStyle(color = Blue600, fontWeight = FontWeight.SemiBold)) {
-                        append("Change Profile Photo")
-                    }
-                },
-                    modifier = Modifier.constrainAs(changePhotoText) {
-                        top.linkTo(profilePictureArea.bottom, margin = 12.dp)
-                        start.linkTo(parent.start)
-                        end.linkTo(parent.end)
-
-                    },
-                    style = MaterialTheme.typography.body2,
-                    onClick = {
-                        event(EditProfileUiEvent.ToggleBottomSheet)
-                    })
-
-
-
-
-
-                TextFieldInputArea(
-                    modifier = Modifier
-                        .constrainAs(textFieldInputArea) {
-                            top.linkTo(changePhotoText.bottom, margin = 30.dp)
-                            start.linkTo(parent.start)
-                            end.linkTo(parent.end)
-                            height = Dimension.percent(0.5f)
-                            width = Dimension.percent(0.9f)
-
-                        },
-                    state = state,
-                    onValueChangeName = { event(EditProfileUiEvent.ChangeName(it)) },
-                    onValueChangePhoneNumber = { event(EditProfileUiEvent.ChangePhoneNumber(it)) },
-                    keyboardActions = keyboardActions,
-                    uiState = uiState,
-                )
-
-
-
-                MappingButtonNavigation(
-                    modifier = Modifier
-                        .constrainAs(buttonNavigationArea) {
-                            top.linkTo(textFieldInputArea.bottom, margin = 20.dp)
-                            start.linkTo(parent.start)
-                            end.linkTo(parent.end)
-                            bottom.linkTo(parent.bottom, margin = 50.dp)
-                            height = Dimension.percent(0.1f)
-                            width = Dimension.percent(0.8f)
-                        },
-                    positiveButtonText = "Save",
-                    onClickCancelButton = { event(EditProfileUiEvent.CancelEditProfile) },
-                    onClickConfirmButton = { event(EditProfileUiEvent.ConfirmEditProfile) },
-                    negativeButtonEnabled = !state.isLoading,
-                    positiveButtonEnabled = !state.isLoading && isUserInformationChanges,
-                )
-
-                if (uiState.noInternetVisible) {
-
-                    NoInternetDialog(
-                        modifier = Modifier.constrainAs(noInternetDialog) {
-                            end.linkTo(parent.end)
-                            start.linkTo(parent.start)
-                            bottom.linkTo(parent.bottom)
-                            width = Dimension.matchParent
-                            height = Dimension.wrapContent
-                        }, onDismiss = { event(EditProfileUiEvent.DismissNoInternetDialog) }
-                    )
-
-                }
-
-                if (state.isLoading) {
-                    CircularProgressIndicator(modifier = Modifier.constrainAs(progressBar) {
-                        top.linkTo(parent.top)
-                        end.linkTo(parent.end)
-                        start.linkTo(parent.start)
-                        bottom.linkTo(parent.bottom)
-                        this.centerTo(parent)
-                    })
-                }
-            }
-        }
-    }
-}
-
 
