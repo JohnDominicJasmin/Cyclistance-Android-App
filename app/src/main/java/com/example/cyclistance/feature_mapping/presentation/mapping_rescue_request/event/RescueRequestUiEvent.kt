f8820e7ef22429d64e21e3e646db0f3a56b94776
John_Dominic <johndominicjasmin@gmail.com>
2023-06-08 01:09:18 +0800

    Remove RescueRequest Screen, UIState, ViewModel and UIEvent

--- app/src/main/java/com/example/cyclistance/feature_mapping/presentation/mapping_rescue_request/event/RescueRequestUiEvent.kt
+++
@@ -1,8 +0,0 @@
-package com.example.cyclistance.feature_mapping.presentation.mapping_rescue_request.event
-
-sealed class RescueRequestUiEvent{
-    data class CancelRequestHelp(val id: String): RescueRequestUiEvent()
-    data class ConfirmRequestHelp(val id: String): RescueRequestUiEvent()
-    object DismissAlertDialog: RescueRequestUiEvent()
-    object DismissNoInternetDialog: RescueRequestUiEvent()
-}
