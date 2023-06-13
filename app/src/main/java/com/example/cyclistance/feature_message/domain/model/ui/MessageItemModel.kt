8b83c53b933aedc3c0860dd06a2729293f456f67
John_Dominic <johndominicjasmin@gmail.com>
2023-06-13 15:20:06 +0800

    Create UI for messaging content

---
+++ app/src/main/java/com/example/cyclistance/feature_message/domain/model/ui/MessageItemModel.kt
@@ -0,0 +1,19 @@
+package com.example.cyclistance.feature_message.domain.model.ui
+
+import android.os.Parcelable
+import com.example.cyclistance.core.utils.annotations.StableState
+import kotlinx.parcelize.Parcelize
+
+
+@StableState
+@Parcelize
+data class MessageItemModel(
+    val messageId: String = "",
+    val userImage: String = "",
+    val name: String = "",
+    val message: String = "",
+    val time: String = "",
+    val unreadMessages: Int = 0,
+    val isMessageSent: Boolean = false,
+
+    ) : Parcelable
---
+++ app/src/main/java/com/example/cyclistance/feature_message/presentation/MessagingScreen.kt
@@ -0,0 +1,8 @@
+package com.example.cyclistance.feature_message.presentation
+
+import androidx.compose.runtime.Composable
+
+@Composable
+fun MessagingScreen() {
+
+}
