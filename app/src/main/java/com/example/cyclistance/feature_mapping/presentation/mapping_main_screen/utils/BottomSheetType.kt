894120b5fba4e878fec6b907723164bd38879135
John_Dominic <johndominicjasmin@gmail.com>
2023-06-26 12:34:35 +0800

    Add ReportIncident to BottomSheetType sealed class

--- app/src/main/java/com/example/cyclistance/feature_mapping/presentation/mapping_main_screen/utils/BottomSheetType.kt
+++ app/src/main/java/com/example/cyclistance/feature_mapping/presentation/mapping_main_screen/utils/BottomSheetType.kt
@@ -3,6 +3,7 @@ package com.example.cyclistance.feature_mapping.presentation.mapping_main_screen
 sealed class BottomSheetType(val type: String) {
     object RescuerArrived : BottomSheetType(type = "rescuer_arrived")
     object DestinationReached : BottomSheetType(type = "destination_reached")
+    object ReportIncident : BottomSheetType(type = "report_incident")
     object SearchAssistance : BottomSheetType(type = "search_assistance")
     object OnGoingRescue : BottomSheetType(type = "on_going_rescue")
     object Collapsed : BottomSheetType(type = "collapsed")
