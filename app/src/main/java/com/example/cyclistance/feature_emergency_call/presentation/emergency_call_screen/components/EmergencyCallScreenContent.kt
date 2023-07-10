62ca4da64a33d86d648ccf67121e559f9d5a7da4
John_Dominic <johndominicjasmin@gmail.com>
2023-07-10 09:59:37 +0800

    Remove CancelButton and move add button inside LazyColumn item

--- app/src/main/java/com/example/cyclistance/feature_emergency_call/presentation/emergency_call_screen/components/EmergencyCallScreenContent.kt
+++ app/src/main/java/com/example/cyclistance/feature_emergency_call/presentation/emergency_call_screen/components/EmergencyCallScreenContent.kt
@@ -1,19 +1,12 @@
 package com.example.cyclistance.feature_emergency_call.presentation.emergency_call_screen.components
 
-import androidx.compose.foundation.BorderStroke
-import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.wrapContentSize
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
-import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.material.ButtonDefaults
 import androidx.compose.material.MaterialTheme
-import androidx.compose.material.OutlinedButton
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
@@ -25,7 +18,6 @@ import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.example.cyclistance.core.utils.constants.EmergencyCallConstants
@@ -62,70 +54,53 @@ fun EmergencyCallScreenContent(
                 )
             }
 
-            Column(verticalArrangement = Arrangement.SpaceEvenly) {
 
 
-                ButtonAddContact(
-                    onClick = { event(EmergencyCallUiEvent.OnClickAddContact) },
-                    modifier = Modifier.padding(vertical = 16.dp))
 
+            LazyColumn(
+                modifier = Modifier
+                    .fillMaxWidth(),
+                horizontalAlignment = Alignment.CenterHorizontally) {
 
-                if (contactsAvailable) {
-
-                    LazyColumn(
-                        modifier = Modifier
-                            .fillMaxWidth()
-                            .weight(0.8f),
-                        horizontalAlignment = Alignment.CenterHorizontally) {
-
-                        items(items = state.emergencyCallModel.contacts, key = { it.id }) { item ->
-                            ContactItem(
-                                onClick = { event(EmergencyCallUiEvent.OnClickContact(item.phoneNumber)) },
-                                emergencyContact = item,
-                                modifier = Modifier
-                                    .fillMaxWidth()
-                                    .padding(vertical = 1.dp),
-                                onClickEdit = { event(EmergencyCallUiEvent.OnClickEditContact(item.id)) },
-                                onClickDelete = {
-                                    event(EmergencyCallUiEvent.OnClickDeleteContact(item))
-                                })
-                        }
-                    }
-
-                } else {
-                    Box(
-                        modifier = Modifier
-                            .fillMaxWidth()
-                            .weight(0.8f), contentAlignment = Alignment.Center) {
-                        Text(
-                            text = "No contacts added",
-                            style = MaterialTheme.typography.h6,
-                            color = Black500
-                        )
+                item {
+                    ButtonAddContact(
+                        onClick = { event(EmergencyCallUiEvent.OnClickAddContact) },
+                        modifier = Modifier.padding(vertical = 16.dp))
+                }
+                items(items = state.emergencyCallModel.contacts, key = { it.id }) { item ->
+                    if (contactsAvailable) {
+                        ContactItem(
+                            onClick = { event(EmergencyCallUiEvent.OnClickContact(item.phoneNumber)) },
+                            emergencyContact = item,
+                            modifier = Modifier
+                                .fillMaxWidth()
+                                .padding(vertical = 1.dp),
+                            onClickEdit = {
+                                event(
+                                    EmergencyCallUiEvent.OnClickEditContact(
+                                        item.id))
+                            },
+                            onClickDelete = {
+                                event(EmergencyCallUiEvent.OnClickDeleteContact(item))
+                            })
                     }
                 }
 
+            }
+
+
+            if (!contactsAvailable) {
                 Box(
                     modifier = Modifier
-                        .weight(0.2f)
-                        .fillMaxWidth(),
-                    contentAlignment = Alignment.Center) {
-
-                    OutlinedButton(
-                        modifier = Modifier.wrapContentSize(),
-                        onClick = { event(EmergencyCallUiEvent.OnClickCancel) },
-                        colors = ButtonDefaults.buttonColors(backgroundColor = Color.Transparent),
-                        border = BorderStroke(1.dp, Black500), shape = RoundedCornerShape(12.dp)) {
-
-                        Text(
-                            text = "Close",
-                            color = MaterialTheme.colors.onBackground,
-                            style = MaterialTheme.typography.button,
-                            modifier = Modifier.padding(horizontal = 24.dp, vertical = 2.dp))
-                    }
-
+                        .fillMaxSize(), contentAlignment = Alignment.Center) {
+                    Text(
+                        text = "No contacts added",
+                        style = MaterialTheme.typography.h6,
+                        color = Black500
+                    )
                 }
             }
+
         }
 
     }
@@ -205,7 +180,7 @@ fun PreviewEmergencyCallScreenContentDark() {
         EmergencyCallScreenContent(
             uiState = uiState,
             state = EmergencyCallState(
-                emergencyCallModel = fakeContacts
+                emergencyCallModel = EmergencyCallModel()
             ),
             event = { event ->
                 when (event) {
