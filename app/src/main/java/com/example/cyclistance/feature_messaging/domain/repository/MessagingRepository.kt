a3e57ae7ed33c1dc5df194f8cb13d6ed506dcca9
John_Dominic <johndominicjasmin@gmail.com>
2023-07-31 21:50:34 +0800

    Add function to addChatListener and removeChatListener

--- app/src/main/java/com/example/cyclistance/feature_messaging/domain/repository/MessagingRepository.kt
+++ app/src/main/java/com/example/cyclistance/feature_messaging/domain/repository/MessagingRepository.kt
@@ -1,6 +1,7 @@
 package com.example.cyclistance.feature_messaging.domain.repository
 
 import com.example.cyclistance.feature_messaging.domain.model.SendMessageModel
+import com.example.cyclistance.feature_messaging.domain.model.ui.chats.ChatsModel
 import com.example.cyclistance.feature_messaging.domain.model.ui.chats.MessagingUserModel
 import com.example.cyclistance.feature_messaging.domain.model.ui.conversation.ConversationsModel
 
@@ -13,6 +14,9 @@ interface MessagingRepository {
     fun getUserUid(): String
     fun addMessageListener(receiverId: String, onNewMessage: (ConversationsModel) -> Unit)
     fun removeMessageListener()
+
+    fun addChatListener(receiverId: String, onNewChat: (ChatsModel) -> Unit)
+    fun removeChatListener()
     suspend fun getConversionId(receiverId: String): String
     fun addConversion(conversion: HashMap<String, Any>, onNewConversionId: (String) -> Unit)
     fun updateConversion(message: String, conversionId: String)
