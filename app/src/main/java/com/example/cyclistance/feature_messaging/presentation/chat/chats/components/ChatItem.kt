affb473491ce602e66af48c7b4858260eda95e25
John_Dominic <johndominicjasmin@gmail.com>
2023-08-02 23:02:48 +0800

    Change AsyncImage to MessageUserImage

--- app/src/main/java/com/example/cyclistance/feature_messaging/presentation/chat/chats/components/ChatItem.kt
+++ app/src/main/java/com/example/cyclistance/feature_messaging/presentation/chat/chats/components/ChatItem.kt
@@ -7,9 +7,8 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.wrapContentHeight
-import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.foundation.layout.wrapContentSize
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.ExperimentalMaterialApi
 import androidx.compose.material.MaterialTheme
@@ -18,24 +17,16 @@ import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.clip
-import androidx.compose.ui.layout.ContentScale
-import androidx.compose.ui.platform.LocalContext
-import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.TextUnit
 import androidx.compose.ui.unit.TextUnitType
 import androidx.compose.ui.unit.dp
-import coil.compose.AsyncImage
-import coil.compose.rememberAsyncImagePainter
-import coil.request.CachePolicy
-import coil.request.ImageRequest
-import com.example.cyclistance.R
 import com.example.cyclistance.core.utils.constants.MappingConstants
 import com.example.cyclistance.core.utils.validation.FormatterUtils.toReadableDateTime
 import com.example.cyclistance.feature_messaging.domain.model.ui.chats.ChatItemModel
+import com.example.cyclistance.feature_messaging.presentation.common.MessageUserImage
 import com.example.cyclistance.theme.CyclistanceTheme
 import java.util.Date
 
@@ -67,22 +58,10 @@ fun ChatItem(
                     .padding(end = 6.dp),
                 contentAlignment = Alignment.CenterStart) {
 
-                AsyncImage(
-                    model = ImageRequest.Builder(LocalContext.current)
-                        .data(chatItemModel.conversionPhoto)
-                        .crossfade(true)
-                        .networkCachePolicy(CachePolicy.ENABLED)
-                        .diskCachePolicy(CachePolicy.ENABLED)
-                        .memoryCachePolicy(CachePolicy.ENABLED)
-                        .build(),
-                    contentDescription = "User Profile Image",
-                    modifier = Modifier
-                        .clip(CircleShape)
-                        .size(54.dp),
-                    contentScale = ContentScale.Crop,
-                    placeholder = rememberAsyncImagePainter(model = R.drawable.ic_empty_profile_placeholder_large),
-                    error = painterResource(id = R.drawable.ic_empty_profile_placeholder_large),
-                    fallback = painterResource(id = R.drawable.ic_empty_profile_placeholder_large)
+                MessageUserImage(
+                    modifier = Modifier.wrapContentSize(),
+                    isOnline = true,
+                    photoUrl = chatItemModel.conversionPhoto
                 )
             }
 
