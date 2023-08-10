c93da01f03b3b5e9560a8b8b69bec2b7aa4d6e09
John_Dominic <johndominicjasmin@gmail.com>
2023-08-10 12:10:51 +0800

    Add ForgotPasswordViewModel

---
+++ app/src/main/java/com/example/cyclistance/feature_authentication/presentation/auth_forgot_password/ForgotPasswordViewModel.kt
@@ -0,0 +1,13 @@
+package com.example.cyclistance.feature_authentication.presentation.auth_forgot_password
+
+import androidx.lifecycle.ViewModel
+import com.example.cyclistance.feature_authentication.presentation.auth_forgot_password.state.ForgotPasswordState
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.asStateFlow
+
+
+class ForgotPasswordViewModel: ViewModel() {
+
+    private val _state = MutableStateFlow(ForgotPasswordState())
+    val state = _state.asStateFlow()
+}
