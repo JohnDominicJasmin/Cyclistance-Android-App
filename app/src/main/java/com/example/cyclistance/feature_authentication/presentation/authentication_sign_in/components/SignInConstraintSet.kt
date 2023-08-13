8c69d3c5843503af48120d20ada82e5f038b5fd0
John_Dominic <johndominicjasmin@gmail.com>
2023-08-13 16:16:17 +0800

    Change width of SignIn AppIcon display

--- app/src/main/java/com/example/cyclistance/feature_authentication/presentation/authentication_sign_in/components/SignInConstraintSet.kt
+++ app/src/main/java/com/example/cyclistance/feature_authentication/presentation/authentication_sign_in/components/SignInConstraintSet.kt
@@ -42,7 +42,7 @@ val signInConstraints = ConstraintSet {
         top.linkTo(topSpacer.bottom, margin = 12.dp)
         start.linkTo(parent.start)
         end.linkTo(parent.end)
-        width = Dimension.percent(0.2f)
+        width = Dimension.percent(0.3f)
         height = Dimension.wrapContent
     }
 
