f6c151ee514a4495070f4bd449204d2efb4e5c70
John_Dominic <johndominicjasmin@gmail.com>
2023-08-13 17:45:18 +0800

    Open terms and condition url and privacy policy in settings screen

--- app/src/main/java/com/example/cyclistance/feature_settings/presentation/setting_screen/components/SettingScreenContent.kt
+++ app/src/main/java/com/example/cyclistance/feature_settings/presentation/setting_screen/components/SettingScreenContent.kt
@@ -1,6 +1,7 @@
 package com.example.cyclistance.feature_settings.presentation.setting_screen.components
 
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
@@ -11,9 +12,13 @@ import androidx.compose.runtime.Composable
 import androidx.compose.runtime.CompositionLocalProvider
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.example.cyclistance.R
+import com.example.cyclistance.core.presentation.dialogs.webview_dialog.DialogWebView
+import com.example.cyclistance.feature_settings.presentation.setting_screen.event.SettingUiEvent
+import com.example.cyclistance.feature_settings.presentation.setting_screen.state.SettingUiState
 import com.example.cyclistance.navigation.IsDarkTheme
 import com.example.cyclistance.theme.CyclistanceTheme
 
@@ -21,87 +26,98 @@ import com.example.cyclistance.theme.CyclistanceTheme
 @Composable
 fun SettingScreenContent(
     modifier: Modifier = Modifier,
-    onToggleTheme: () -> Unit = {},
-    onClickEditProfile: () -> Unit = {},
-    onClickChangePassword: () -> Unit = {},
-    ) {
+    uiState: SettingUiState,
+    event: (SettingUiEvent) -> Unit,
+) {
 
     val isDarkTheme = IsDarkTheme.current
+    val context = LocalContext.current
     Surface(modifier = modifier, color = MaterialTheme.colors.background) {
-
-        Column(
-            modifier = Modifier
-                .fillMaxSize(),
-            verticalArrangement = Arrangement.Top,
-            horizontalAlignment = Alignment.CenterHorizontally) {
+        Box(modifier = Modifier.fillMaxSize(), contentAlignment = Alignment.Center) {
 
             Column(
                 modifier = Modifier
-                    .fillMaxWidth(0.9f)
-                    .padding(top = 30.dp),
-                verticalArrangement = Arrangement.spacedBy(3.dp)) {
+                    .fillMaxSize(),
+                verticalArrangement = Arrangement.Top,
+                horizontalAlignment = Alignment.CenterHorizontally) {
 
-                SectionTitle(iconId = R.drawable.ic_baseline_brush_24, title = "Preference")
-                SettingSwitchButton(
-                    checkedState = isDarkTheme,
-                    onCheckedChange = { onToggleTheme() },
-                    onClick = onToggleTheme)
+                Column(
+                    modifier = Modifier
+                        .fillMaxWidth(0.9f)
+                        .padding(top = 30.dp),
+                    verticalArrangement = Arrangement.spacedBy(3.dp)) {
+
+                    SectionTitle(iconId = R.drawable.ic_baseline_brush_24, title = "Preference")
+                    SettingSwitchButton(
+                        checkedState = isDarkTheme,
+                        onCheckedChange = { event(SettingUiEvent.ClickToggleTheme) },
+                        onClick = { event(SettingUiEvent.ClickToggleTheme) })
 
 
+                }
+
+                Column(
+                    modifier = Modifier
+                        .fillMaxWidth(0.9f)
+                        .padding(top = 30.dp),
+                    verticalArrangement = Arrangement.spacedBy(3.dp)) {
+
+
+                    SectionTitle(iconId = R.drawable.ic_baseline_person_24, title = "Account")
+                    SettingsButtonItem(
+                        buttonText = "Edit Profile",
+                        onClick = { event(SettingUiEvent.ClickEditProfile) })
+
+                }
+
+
+                Column(
+                    modifier = Modifier
+                        .fillMaxWidth(0.9f)
+                        .padding(top = 30.dp),
+                    verticalArrangement = Arrangement.spacedBy(3.dp)) {
+
+
+                    SectionTitle(iconId = R.drawable.baseline_lock_24, title = "Security")
+                    SettingsButtonItem(
+                        buttonText = "Change Password",
+                        onClick = { event(SettingUiEvent.ClickResetPassword) })
+
+                }
+
+
+                Column(
+                    modifier = Modifier
+                        .fillMaxWidth(0.9f)
+                        .padding(top = 30.dp),
+                    verticalArrangement = Arrangement.spacedBy(3.dp)) {
+
+                    SectionTitle(
+                        iconId = R.drawable.ic_baseline_settings_suggest_24,
+                        title = "Other Settings")
+
+                    SettingsButtonItem(buttonText = "Privacy Policy", onClick = {
+                        event(SettingUiEvent.OpenWebView(context.getString(R.string.privacy_policy_url)))
+                    })
+
+                    SettingsButtonItem(buttonText = "Terms and Condition", onClick = {
+                        event(SettingUiEvent.OpenWebView(context.getString(R.string.terms_and_condition_url)))
+                    })
+
+                    SettingsButtonItem(buttonText = "Rate this app", onClick = {
+
+                    })
+
+
+                }
             }
 
-            Column(
-                modifier = Modifier
-                    .fillMaxWidth(0.9f)
-                    .padding(top = 30.dp),
-                verticalArrangement = Arrangement.spacedBy(3.dp)) {
 
-
-                SectionTitle(iconId = R.drawable.ic_baseline_person_24, title = "Account")
-                SettingsButtonItem(buttonText = "Edit Profile", onClick = onClickEditProfile)
-
+            if(uiState.urlToOpen != null){
+                DialogWebView(modifier = Modifier.fillMaxSize(), mUrl = uiState.urlToOpen, onDismiss = {event(SettingUiEvent.DismissWebView)})
             }
 
-
-            Column(
-                modifier = Modifier
-                    .fillMaxWidth(0.9f)
-                    .padding(top = 30.dp),
-                verticalArrangement = Arrangement.spacedBy(3.dp)) {
-
-
-                SectionTitle(iconId = R.drawable.baseline_lock_24, title = "Security")
-                SettingsButtonItem(buttonText = "Change Password", onClick = onClickChangePassword)
-
-            }
-
-
-            Column(
-                modifier = Modifier
-                    .fillMaxWidth(0.9f)
-                    .padding(top = 30.dp),
-                verticalArrangement = Arrangement.spacedBy(3.dp)) {
-
-                SectionTitle(
-                    iconId = R.drawable.ic_baseline_settings_suggest_24,
-                    title = "Other Settings")
-
-                SettingsButtonItem(buttonText = "Privacy Policy", onClick = {
-
-                })
-
-                SettingsButtonItem(buttonText = "Terms and Condition", onClick = {
-
-                })
-
-                SettingsButtonItem(buttonText = "Rate this app", onClick = {
-
-                })
-
-
-            }
         }
-
     }
 }
 
@@ -112,7 +128,8 @@ fun PreviewSettingScreenDark() {
         CyclistanceTheme(true) {
 
             SettingScreenContent(
-                onToggleTheme = {},
+                uiState = SettingUiState(),
+                event = {}
             )
         }
     }
@@ -126,7 +143,8 @@ fun PreviewSettingScreenLight() {
         CyclistanceTheme(false) {
 
             SettingScreenContent(
-                onToggleTheme = {},
+                uiState = SettingUiState(),
+                event = {}
             )
         }
     }
--- app/src/main/java/com/example/cyclistance/feature_settings/presentation/setting_screen/event/SettingUiEvent.kt
+++ app/src/main/java/com/example/cyclistance/feature_settings/presentation/setting_screen/event/SettingUiEvent.kt
@@ -1,6 +1,10 @@
 package com.example.cyclistance.feature_settings.presentation.setting_screen.event
 
-sealed class SettingUiEvent {
-    object SignOutSuccess: SettingUiEvent()
-    object SignOutFailed: SettingUiEvent()
+sealed class SettingUiEvent{
+    object ClickToggleTheme: SettingUiEvent()
+    object ClickResetPassword: SettingUiEvent()
+    object ClickEditProfile: SettingUiEvent()
+    object DismissWebView: SettingUiEvent()
+    data class OpenWebView(val url: String): SettingUiEvent()
+
 }
