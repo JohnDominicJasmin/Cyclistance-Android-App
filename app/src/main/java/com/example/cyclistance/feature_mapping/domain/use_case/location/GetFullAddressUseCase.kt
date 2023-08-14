1550bfe87f6455746a1b084fe9c815aa0ac90525
John_Dominic <johndominicjasmin@gmail.com>
2023-08-14 17:35:38 +0800

    Set latitude and longitude nullable and add null check

--- app/src/main/java/com/example/cyclistance/feature_mapping/domain/use_case/location/GetFullAddressUseCase.kt
+++ app/src/main/java/com/example/cyclistance/feature_mapping/domain/use_case/location/GetFullAddressUseCase.kt
@@ -3,9 +3,12 @@ package com.example.cyclistance.feature_mapping.domain.use_case.location
 import com.example.cyclistance.feature_mapping.domain.repository.MappingRepository
 
 class GetFullAddressUseCase(private val repository: MappingRepository) {
-    suspend operator fun invoke(latitude: Double, longitude: Double): String =
-        repository.getFullAddress(
+    suspend operator fun invoke(latitude: Double?, longitude: Double?): String? {
+        latitude ?: return null
+        longitude ?: return null
+        return repository.getFullAddress(
             latitude = latitude,
             longitude = longitude
         )
+    }
 }
