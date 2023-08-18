22d0142d4f2a803f9e8853573e42a3b102c4da94
John_Dominic <johndominicjasmin@gmail.com>
2023-08-18 23:31:13 +0800

    Add HazardousLane state in MappingState

--- app/src/main/java/com/example/cyclistance/feature_mapping/presentation/mapping_main_screen/state/MappingState.kt
+++ app/src/main/java/com/example/cyclistance/feature_mapping/presentation/mapping_main_screen/state/MappingState.kt
@@ -2,6 +2,7 @@ package com.example.cyclistance.feature_mapping.presentation.mapping_main_screen
 
 import android.os.Parcelable
 import com.example.cyclistance.core.utils.annotations.StableState
+import com.example.cyclistance.feature_mapping.domain.model.remote_models.hazardous_lane.HazardousLane
 import com.example.cyclistance.feature_mapping.domain.model.remote_models.rescue_transaction.RescueTransactionItem
 import com.example.cyclistance.feature_mapping.domain.model.remote_models.user.LocationModel
 import com.example.cyclistance.feature_mapping.domain.model.remote_models.user.UserItem
@@ -25,6 +26,7 @@ data class MappingState(
     val rescuerDistance: String = "",
     val newRescueRequest: NewRescueRequestsModel? = null,
     val speedometerState: SpeedometerState = SpeedometerState(),
+    val hazardousLane: HazardousLane? = null,
 ) : Parcelable{
 
 
