1873346de7a354b65f2d798ea61f36ca22b0ed09
John_Dominic <johndominicjasmin@gmail.com>
2023-08-21 21:56:12 +0800

    Remove WebSocket client requests of Hazardous lane

--- app/src/main/java/com/example/cyclistance/feature_mapping/domain/repository/MappingSocketRepository.kt
+++ app/src/main/java/com/example/cyclistance/feature_mapping/domain/repository/MappingSocketRepository.kt
@@ -10,12 +10,10 @@ interface MappingSocketRepository {
     suspend fun getUserUpdates(): Flow<NearbyCyclist>
     suspend fun getRescueTransactionUpdates(): Flow<RescueTransaction>
     suspend fun getTransactionLocationUpdates(): Flow<LiveLocationSocketModel>
-    suspend fun getNewHazardousLaneUpdates(): Flow<HazardousLane>
+
 
     suspend fun broadcastToNearbyCyclists(locationModel: LiveLocationSocketModel)
     suspend fun broadcastRescueTransactionToRespondent()
-    suspend fun addNewHazardousLane(hazardousLaneMarker: HazardousLaneMarker)
-    suspend fun requestHazardousLane()
-    suspend fun deleteHazardousLane(id: String)
+
     suspend fun broadcastTransactionLocation(locationModel: LiveLocationSocketModel)
 }
