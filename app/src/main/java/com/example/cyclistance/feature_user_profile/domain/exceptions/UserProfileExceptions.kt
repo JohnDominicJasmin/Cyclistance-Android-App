6858ae4d9567c5879a18e23acdac8c32664b8089
John_Dominic <johndominicjasmin@gmail.com>
2023-08-31 19:39:11 +0800

    Add AddressException and UpdateProfileException

--- app/src/main/java/com/example/cyclistance/feature_user_profile/domain/exceptions/UserProfileExceptions.kt
+++ app/src/main/java/com/example/cyclistance/feature_user_profile/domain/exceptions/UserProfileExceptions.kt
@@ -2,6 +2,8 @@ package com.example.cyclistance.feature_user_profile.domain.exceptions
 
 sealed class UserProfileExceptions{
     class NameException(message: String = "Field cannot be blank"):RuntimeException(message)
+    class AddressException(message: String = "Field cannot be blank") : RuntimeException(message)
     class NetworkException(message: String) : RuntimeException(message)
     class InternalServerException(message: String) : RuntimeException(message)
+    class UpdateProfileException(message: String) : RuntimeException(message)
 }
