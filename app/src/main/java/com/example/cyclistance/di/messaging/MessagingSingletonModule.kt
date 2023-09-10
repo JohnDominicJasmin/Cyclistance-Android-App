12904d316680e7cd2e2864b5cef1214bbefbdf15
John_Dominic <johndominicjasmin@gmail.com>
2023-09-11 00:08:51 +0800

    Move NotificationCompatBuilder and NotificationChannel in MessagingSingletonModule

--- app/src/main/java/com/example/cyclistance/di/messaging/MessagingSingletonModule.kt
+++ app/src/main/java/com/example/cyclistance/di/messaging/MessagingSingletonModule.kt
@@ -1,7 +1,13 @@
 package com.example.cyclistance.di.messaging
 
+import android.app.NotificationChannel
+import android.app.NotificationManager
 import android.content.Context
+import android.os.Build
+import androidx.annotation.RequiresApi
+import androidx.core.app.NotificationCompat
 import com.example.cyclistance.R
+import com.example.cyclistance.core.utils.constants.MessagingConstants
 import com.example.cyclistance.feature_messaging.data.MessagingApi
 import com.google.firebase.messaging.FirebaseMessaging
 import dagger.Module
@@ -11,6 +17,7 @@ import dagger.hilt.android.qualifiers.ApplicationContext
 import dagger.hilt.components.SingletonComponent
 import retrofit2.Retrofit
 import retrofit2.converter.scalars.ScalarsConverterFactory
+import javax.inject.Named
 import javax.inject.Singleton
 
 
@@ -37,6 +44,42 @@ object MessagingSingletonModule {
                 .addConverterFactory(ScalarsConverterFactory.create())
                 .build()
                 .create(MessagingApi::class.java)
+        }.value
+    }
+
+    @Provides
+    @Singleton
+    @Named("messagingNotification")
+    fun provideMessagingNotification(
+        @ApplicationContext context: Context
+    ): NotificationCompat.Builder{
+        return lazy {
+            NotificationCompat.Builder(
+                context,
+                MessagingConstants.CHANNEL_ID).apply {
+                setSmallIcon(R.drawable.ic_app_icon_cyclistance)
+                priority = NotificationCompat.PRIORITY_HIGH
+                setAutoCancel(true)
+                setShowWhen(true)
+                setVisibility(NotificationCompat.VISIBILITY_PRIVATE)
+            }
+
+        }.value
+    }
+
+    @Provides
+    @Singleton
+    @Named("messagingNotificationChannel")
+    @RequiresApi(Build.VERSION_CODES.O)
+    fun provideMessagingNotificationChannel(): NotificationChannel {
+        return lazy {
+            NotificationChannel(
+                MessagingConstants.CHANNEL_ID,
+                MessagingConstants.CHANNEL_NAME,
+                NotificationManager.IMPORTANCE_HIGH).apply {
+                description = MessagingConstants.CHANNEL_DESCRIPTION
+
+            }
         }.value
     }
 
--- app/src/main/java/com/example/cyclistance/service/MessagingService.kt
+++ app/src/main/java/com/example/cyclistance/service/MessagingService.kt
@@ -8,11 +8,9 @@ import androidx.core.app.NotificationCompat
 import androidx.core.app.NotificationManagerCompat
 import androidx.core.net.toUri
 import com.example.cyclistance.MainActivity
-import com.example.cyclistance.R
-import com.example.cyclistance.core.utils.constants.MessagingConstants
 import com.example.cyclistance.core.utils.constants.MessagingConstants.KEY_MESSAGE
-import com.example.cyclistance.core.utils.constants.MessagingConstants.MESSAGING_NOTIFICATION_ID
 import com.example.cyclistance.core.utils.constants.MessagingConstants.MESSAGING_URI
+import com.example.cyclistance.core.utils.constants.MessagingConstants.NOTIFICATION_ID
 import com.example.cyclistance.core.utils.constants.MessagingConstants.RECEIVER_MESSAGE_OBJ
 import com.example.cyclistance.core.utils.constants.MessagingConstants.SENDER_MESSAGE_OBJ
 import com.example.cyclistance.core.utils.constants.UtilConstants.KEY_NAME
@@ -20,10 +18,15 @@ import com.google.firebase.messaging.FirebaseMessagingService
 import com.google.firebase.messaging.RemoteMessage
 import timber.log.Timber
 import javax.inject.Inject
+import javax.inject.Named
 
 class MessagingService @Inject constructor(
-
+    @Named("messagingNotification") private val notificationBuilder: NotificationCompat.Builder,
+    private val notificationManagerCompat: NotificationManagerCompat
 ) : FirebaseMessagingService() {
+
+
+
     override fun onNewToken(token: String) {
         super.onNewToken(token)
         Timber.v("FirebaseMessagingService New token: $token")
@@ -34,28 +37,15 @@ class MessagingService @Inject constructor(
     override fun onMessageReceived(message: RemoteMessage) {
         super.onMessageReceived(message)
 
-
-
-        //need to swap the sender and receiver
         val data = message.data
         val userReceiverMessage = data[RECEIVER_MESSAGE_OBJ]!!
         val userSenderMessage = data[SENDER_MESSAGE_OBJ]!!
         val name = data[KEY_NAME]!!
-        val message = data[KEY_MESSAGE] ?: ""
+        val receivedMessage = data[KEY_MESSAGE] ?: ""
 
 
-
-        val notificationBuilder = NotificationCompat.Builder(
-            this,
-            MessagingConstants.MESSAGING_NOTIFICATION_CHANNEL).apply {
-            setSmallIcon(R.drawable.ic_app_icon_cyclistance)
-            priority = NotificationCompat.PRIORITY_HIGH
-            setAutoCancel(true)
-            setShowWhen(true)
-            setVisibility(NotificationCompat.VISIBILITY_PRIVATE)
-        }
-
-        val uri = "$MESSAGING_URI/$RECEIVER_MESSAGE_OBJ=$userSenderMessage&$SENDER_MESSAGE_OBJ=$userReceiverMessage".toUri()
+        val uri =
+            "$MESSAGING_URI/$RECEIVER_MESSAGE_OBJ=$userSenderMessage&$SENDER_MESSAGE_OBJ=$userReceiverMessage".toUri()
         val clickIntent = Intent(
             Intent.ACTION_VIEW,
             uri,
@@ -67,17 +57,16 @@ class MessagingService @Inject constructor(
             getPendingIntent(1, PendingIntent.FLAG_MUTABLE or PendingIntent.FLAG_UPDATE_CURRENT)
         }
 
-        val notificationStyle = NotificationCompat.BigTextStyle().bigText(message)
+        val notificationStyle = NotificationCompat.BigTextStyle().bigText(receivedMessage)
         val notificationCompat = notificationBuilder.apply {
             setContentIntent(clickPendingIntent)
             setContentTitle("Message from $name")
-            setContentText(message)
+            setContentText(receivedMessage)
             setStyle(notificationStyle)
         }
 
 
-        val notificationManagerCompat = NotificationManagerCompat.from(this)
-        notificationManagerCompat.notify(MESSAGING_NOTIFICATION_ID, notificationCompat.build())
+        notificationManagerCompat.notify(NOTIFICATION_ID, notificationCompat.build())
 
 
     }
