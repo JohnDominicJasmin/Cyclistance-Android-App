2b0bc6f68e32ee16cfd6e77718aba68b14119b7f
John_Dominic <johndominicjasmin@gmail.com>
2023-09-19 15:01:57 +0800

    Delete unused dto

--- app/src/main/java/com/example/cyclistance/feature_mapping/data/data_source/network/dto/hazardous_lane/HazardousLaneDto.kt
+++
@@ -1,5 +0,0 @@
-package com.example.cyclistance.feature_mapping.data.data_source.network.dto.hazardous_lane
-
-import com.example.cyclistance.feature_mapping.domain.model.remote_models.hazardous_lane.HazardousLaneMarker
-
-class HazardousLaneDto: ArrayList<HazardousLaneMarker>()
