a36c8b48596ec05b1d30b29a63d7830462006ebf
John_Dominic <johndominicjasmin@gmail.com>
2023-09-20 15:36:10 +0800

    Add lambda and collect eventFlow

--- app/src/main/java/com/example/cyclistance/feature_rescue_record/presentation/rescue_results/RescueResultsScreen.kt
+++ app/src/main/java/com/example/cyclistance/feature_rescue_record/presentation/rescue_results/RescueResultsScreen.kt
@@ -1,19 +1,24 @@
 package com.example.cyclistance.feature_rescue_record.presentation.rescue_results
 
+import android.widget.Toast
 import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.padding
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
 import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.lifecycle.compose.collectAsStateWithLifecycle
 import androidx.navigation.NavController
 import com.example.cyclistance.feature_rescue_record.presentation.rescue_results.components.RescueResultsScreenContent
+import com.example.cyclistance.feature_rescue_record.presentation.rescue_results.event.RescueResultEvent
 import com.example.cyclistance.feature_rescue_record.presentation.rescue_results.event.RescueResultUiEvent
+import com.example.cyclistance.feature_rescue_record.presentation.rescue_results.event.RescueResultVmEvent
 import com.example.cyclistance.feature_rescue_record.presentation.rescue_results.state.RescueResultUiState
 import com.example.cyclistance.navigation.Screens
 import com.example.cyclistance.navigation.nav_graph.navigateScreen
@@ -26,6 +31,7 @@ fun RescueResultsScreen(
 
 ) {
 
+    val context = LocalContext.current
     var uiState by rememberSaveable{ mutableStateOf(RescueResultUiState()) }
     val state by viewModel.state.collectAsStateWithLifecycle()
 
@@ -49,6 +55,32 @@ fun RescueResultsScreen(
         navController.navigateScreen(route = Screens.UserProfileNavigation.UserProfile.passArgument(userId = userId))
     }}
 
+    val reportAccount = remember {{ id: String, name: String, photo: String ->
+        navController.navigateScreen(
+            route = Screens.ReportAccountNavigation.ReportAccount.passArgument(
+                userId = id,
+                name = name,
+                userPhoto = photo))
+
+    }}
+
+    val rateRescuer = remember{{
+        viewModel.onEvent(event = RescueResultVmEvent.RateRescuer(
+            rating = uiState.rating
+        ))
+    }}
+
+    LaunchedEffect(key1 = true){
+        viewModel.eventFlow.collect{ event ->
+            when(event){
+                is RescueResultEvent.RatingFailed -> {
+                    Toast.makeText(context, event.message, Toast.LENGTH_SHORT).show()
+                }
+                RescueResultEvent.RatingSuccess -> stepUp()
+            }
+        }
+    }
+
     RescueResultsScreenContent(
         uiState = uiState,
         state = state,
@@ -59,7 +91,7 @@ fun RescueResultsScreen(
                 is RescueResultUiEvent.ChangeRating -> changeRating(event.rating)
                 RescueResultUiEvent.StepDown -> stepDown()
                 RescueResultUiEvent.StepUp -> stepUp()
-                is RescueResultUiEvent.ReportAccount -> {}
+                is RescueResultUiEvent.ReportAccount -> reportAccount(event.id, event.name, event.photo)
                 is RescueResultUiEvent.ViewProfile -> viewProfile(event.id)
                 is RescueResultUiEvent.RateRescuer -> rateRescuer()
             }
