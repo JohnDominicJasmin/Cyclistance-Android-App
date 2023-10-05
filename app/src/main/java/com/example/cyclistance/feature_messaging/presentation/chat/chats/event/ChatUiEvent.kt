3ddebef4ce7d9e2a52e82aa454a288f2dbe9ef74
John_Dominic <johndominicjasmin@gmail.com>
2023-10-05 21:57:43 +0800

    Remove Chat in Messaging

--- app/src/main/java/com/example/cyclistance/feature_messaging/presentation/chat/chats/event/ChatUiEvent.kt
+++
@@ -1,8 +0,0 @@
-package com.example.cyclistance.feature_messaging.presentation.chat.chats.event
-
-import com.example.cyclistance.feature_messaging.domain.model.ui.chats.MessagingUserItemModel
-
-sealed class ChatUiEvent {
-    data class OnSelectConversation(val user: MessagingUserItemModel) : ChatUiEvent()
-    object OnRefreshChat: ChatUiEvent()
-}
