5c38d6026dfad867ba3da6b2e2926bc0b07161dd
John_Dominic <johndominicjasmin@gmail.com>
2023-11-27 21:38:04 +0800

    Add composable function for IncidentDetailsScreen and IncidentImageScreen

--- app/src/main/java/com/myapp/cyclistance/navigation/nav_graph/MappingGraph.kt
+++ app/src/main/java/com/myapp/cyclistance/navigation/nav_graph/MappingGraph.kt
@@ -9,16 +9,22 @@ import androidx.navigation.compose.composable
 import androidx.navigation.navArgument
 import androidx.navigation.navigation
 import com.google.accompanist.permissions.ExperimentalPermissionsApi
+import com.myapp.cyclistance.core.utils.JsonConverter.fromJson
 import com.myapp.cyclistance.core.utils.constants.MappingConstants.SELECTION_RESCUEE_TYPE
 import com.myapp.cyclistance.core.utils.constants.NavigationConstants.CANCELLATION_TYPE
 import com.myapp.cyclistance.core.utils.constants.NavigationConstants.CLIENT_ID
+import com.myapp.cyclistance.core.utils.constants.NavigationConstants.INCIDENT_IMAGE_URI
 import com.myapp.cyclistance.core.utils.constants.NavigationConstants.LATITUDE
 import com.myapp.cyclistance.core.utils.constants.NavigationConstants.LONGITUDE
+import com.myapp.cyclistance.core.utils.constants.NavigationConstants.MARKER_DETAILS_OBJECT
 import com.myapp.cyclistance.core.utils.constants.NavigationConstants.TRANSACTION_ID
+import com.myapp.cyclistance.feature_mapping.domain.model.remote_models.hazardous_lane.HazardousLaneMarkerDetails
 import com.myapp.cyclistance.feature_mapping.presentation.mapping_cancellation_reason.CancellationReasonScreen
 import com.myapp.cyclistance.feature_mapping.presentation.mapping_confirm_details.ConfirmDetailsScreen
 import com.myapp.cyclistance.feature_mapping.presentation.mapping_main_screen.MappingScreen
+import com.myapp.cyclistance.feature_mapping.presentation.mapping_marker_incident_details.IncidentDetailsScreen
 import com.myapp.cyclistance.feature_mapping.presentation.mapping_sinotrack.SinoTrackScreen
+import com.myapp.cyclistance.feature_mapping.presentation.mapping_view_image.IncidentImageScreen
 import com.myapp.cyclistance.navigation.Screens
 
 @OptIn(ExperimentalPermissionsApi::class)
@@ -46,6 +52,39 @@ fun NavGraphBuilder.mappingGraph(
         }
 
 
+        composable(route = Screens.MappingNavigation.MarkerIncidentDetails.screenRoute,
+            arguments = listOf(
+                navArgument(MARKER_DETAILS_OBJECT) {
+                    defaultValue = null
+                    nullable = true
+                }
+            )) {
+            val markerDetails = it.arguments?.getString(MARKER_DETAILS_OBJECT)
+                ?.fromJson(type = HazardousLaneMarkerDetails::class.java)!!
+
+            IncidentDetailsScreen(
+                navController = navController,
+                paddingValues = paddingValues,
+                markerDetails = markerDetails)
+        }
+
+        composable(route = Screens.MappingNavigation.IncidentImage.screenRoute, arguments = listOf(
+            navArgument(INCIDENT_IMAGE_URI){
+                type = NavType.StringType
+            })){
+
+            it.arguments?.getString(INCIDENT_IMAGE_URI)?.let { incidentImageUri ->
+
+                IncidentImageScreen(
+                    navController = navController,
+                    paddingValues = paddingValues,
+                    photoUrl = incidentImageUri)
+            }
+
+        }
+
+
+
         composable(
             route = Screens.MappingNavigation.Cancellation.screenRoute,
             arguments = listOf(
@@ -65,6 +104,7 @@ fun NavGraphBuilder.mappingGraph(
         }
 
 
+
         composable(route = Screens.MappingNavigation.ConfirmDetails.screenRoute,
             arguments = listOf(
                 navArgument(LATITUDE) {
@@ -81,13 +121,12 @@ fun NavGraphBuilder.mappingGraph(
 
         }
 
-        composable(route = Screens.MappingNavigation.SinoTrack.screenRoute){
+        composable(route = Screens.MappingNavigation.SinoTrack.screenRoute) {
             SinoTrackScreen(
                 paddingValues = paddingValues,
                 navController = navController
             )
         }
-
 
 
     }
