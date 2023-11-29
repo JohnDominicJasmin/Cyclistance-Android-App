5db10aa8fe2c17dcbf9b654d91abc0996f485c8d
John_Dominic <johndominicjasmin@gmail.com>
2023-11-29 20:36:20 +0800

    Put bitmap to remember block

--- app/src/main/java/com/myapp/cyclistance/feature_mapping/presentation/mapping_view_image/IncidentImageScreen.kt
+++ app/src/main/java/com/myapp/cyclistance/feature_mapping/presentation/mapping_view_image/IncidentImageScreen.kt
@@ -3,6 +3,7 @@ package com.myapp.cyclistance.feature_mapping.presentation.mapping_view_image
 import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.padding
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.asImageBitmap
 import androidx.navigation.NavController
@@ -14,8 +15,12 @@ fun IncidentImageScreen(
     navController: NavController,
     paddingValues: PaddingValues,
     photoUrl: String) {
+    val bitmap = remember { ImageUtils.decodeImage(photoUrl) }
 
-    IncidentImageContent(
-        modifier = Modifier.padding(paddingValues),
-        photoBitmap = ImageUtils.decodeImage(photoUrl).asImageBitmap())
+
+        IncidentImageContent(
+            modifier = Modifier.padding(paddingValues),
+            photoBitmap = bitmap.asImageBitmap()
+        )
+
 }
