eed0c41a8a15476bab4aa99c4fce1553d1bd4867
John_Dominic <johndominicjasmin@gmail.com>
2023-12-15 10:23:53 +0800

    Revert "Revert "Change to non suspend function""
    This reverts commit 01d7d60903a71bc5f83cfbe9a61a2e232bec3890.

--- app/src/main/java/com/myapp/cyclistance/feature_rescue_record/domain/use_case/ride_details/RideDetailsUseCase.kt
+++ app/src/main/java/com/myapp/cyclistance/feature_rescue_record/domain/use_case/ride_details/RideDetailsUseCase.kt
@@ -2,12 +2,14 @@ package com.myapp.cyclistance.feature_rescue_record.domain.use_case.ride_details
 
 import com.myapp.cyclistance.feature_rescue_record.domain.model.ui.RideDetails
 import com.myapp.cyclistance.feature_rescue_record.domain.repository.RescueRecordRepository
+import kotlinx.coroutines.flow.Flow
 
 class RideDetailsUseCase(private val repository: RescueRecordRepository) {
-    suspend operator fun invoke() {
-        repository.getRideDetails()
+    operator fun invoke(): Flow<List<RideDetails>> {
+        return repository.getRideDetails()
     }
-    suspend fun invoke(rideDetails: RideDetails) {
+
+    suspend operator fun invoke(rideDetails: RideDetails) {
         repository.upsertRideDetails(rideDetails = rideDetails)
     }
 }
