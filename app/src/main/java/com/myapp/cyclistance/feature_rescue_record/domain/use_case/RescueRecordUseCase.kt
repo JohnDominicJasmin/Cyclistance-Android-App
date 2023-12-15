8d81087ff6d55863dda646651d4d7bf1791b37ca
John_Dominic <johndominicjasmin@gmail.com>
2023-12-15 10:23:51 +0800

    Revert "Revert "Add useCase for rideDetails and rideMetrics""
    This reverts commit b49a60de2727135f04cbc90bf2e2fce8b765c4d7.

--- app/src/main/java/com/myapp/cyclistance/feature_rescue_record/domain/use_case/RescueRecordUseCase.kt
+++ app/src/main/java/com/myapp/cyclistance/feature_rescue_record/domain/use_case/RescueRecordUseCase.kt
@@ -1,8 +1,12 @@
-package com.myapp.cyclistance.feature_rescue_record.domain.use_case.rescue_record
+package com.myapp.cyclistance.feature_rescue_record.domain.use_case
 
 import com.myapp.cyclistance.feature_rescue_record.domain.use_case.metrics.AddRideMetricsUseCase
 import com.myapp.cyclistance.feature_rescue_record.domain.use_case.rate.RateRescueUseCase
 import com.myapp.cyclistance.feature_rescue_record.domain.use_case.rate.RateRescuerUseCase
+import com.myapp.cyclistance.feature_rescue_record.domain.use_case.rescue_record.AddRescueRecordUseCase
+import com.myapp.cyclistance.feature_rescue_record.domain.use_case.rescue_record.GetRescueRecordUseCase
+import com.myapp.cyclistance.feature_rescue_record.domain.use_case.ride_details.RideDetailsUseCase
+import com.myapp.cyclistance.feature_rescue_record.domain.use_case.ride_metrics.RideMetricsUseCase
 import com.myapp.cyclistance.feature_rescue_record.domain.use_case.ridehistory.GetRideHistoryUseCase
 import com.myapp.cyclistance.feature_rescue_record.domain.use_case.stats.UpdateStatsUseCase
 
@@ -14,4 +18,6 @@ data class RescueRecordUseCase(
     val rateRescuerUseCase: RateRescuerUseCase,
     val updateStatsUseCase: UpdateStatsUseCase,
     val addRideMetricsUseCase: AddRideMetricsUseCase,
+    val rideDetailsUseCase: RideDetailsUseCase,
+    val rideMetricsUseCase: RideMetricsUseCase
 )
