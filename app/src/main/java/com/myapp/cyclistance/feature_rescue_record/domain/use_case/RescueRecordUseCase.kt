8e44ee0d9d5e44829c2dd2d2fa6c840404e977c0
John_Dominic <johndominicjasmin@gmail.com>
2023-12-15 10:23:50 +0800

    Revert "Revert "Remove flow repository and its use case""
    This reverts commit 30e9c8ddb04535f9a62ddc95f0da1351c0eb1f05.

--- app/src/main/java/com/myapp/cyclistance/feature_rescue_record/domain/use_case/RescueRecordUseCase.kt
+++ app/src/main/java/com/myapp/cyclistance/feature_rescue_record/domain/use_case/RescueRecordUseCase.kt
@@ -2,12 +2,10 @@ package com.myapp.cyclistance.feature_rescue_record.domain.use_case
 
 data class RescueRecordUseCase(
     val addRescueRecordUseCase: AddRescueRecordUseCase,
-    val rescueDetailsUseCase: RescueDetailsUseCase,
     val getRescueRecordUseCase: GetRescueRecordUseCase,
     val getRideHistoryUseCase: GetRideHistoryUseCase,
     val rateRescueUseCase: RateRescueUseCase,
     val rateRescuerUseCase: RateRescuerUseCase,
     val updateStatsUseCase: UpdateStatsUseCase,
     val addRideMetricsUseCase: AddRideMetricsUseCase,
-    val rideMetricsUseCase: RideMetricsUseCase
 )
